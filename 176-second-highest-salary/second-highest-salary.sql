10f5f60e16fce51182da89f29a0fa9d947bd789a
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2024-10-09 12:26:30 -0700

    Time: 440 ms (7.07%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,3 +1,13 @@
-SELECT MAX(salary) AS SecondHighestSalary 
-FROM Employee
-WHERE salary < (SELECT MAX(salary) FROM Employee);
+
+-- select e.salary as SecondHighestSalary from Employee e 
+-- where max(e.salary) < (select max(salary) from employee)
+
+select max(x.salary) as SecondHighestSalary from (
+select * from Employee o 
+where  o.salary < (select max(salary) from employee)
+
+) as x
+
+
+-- select e.salary as SecondHighestSalary from employee e
+-- where e.id = (select id from employee order by salary desc limit 1 offset 1)
