2643b4b6d27d4d61e5601bef27bfb3d66168d958
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2024-10-17 20:31:43 -0700

    Time: 2 ms (99.88%) | Memory: 10.1 MB (82.14%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,7 +1,10 @@
 class Solution {
 private: 
 int f(int i, int n, vector<int>&nums,int maxi, int curor ){
-    if(i>=n) return curor==maxi;
+    if(curor==maxi){
+         return 1<<(nums.size()-i);
+    } 
+    if(i>=n) return 0;
     int tmp= curor | nums[i];
     int take=f(i+1,n,nums,maxi,tmp);
     int ntake=f(i+1, n, nums, maxi, curor);
