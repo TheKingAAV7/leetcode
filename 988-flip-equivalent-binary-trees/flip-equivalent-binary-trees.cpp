ba392c7cd4826b63e286db0c3d7fa7cf65a99be9
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2024-10-24 09:18:37 -0700

    Time: 0 ms (100.00%) | Memory: 16.5 MB (6.32%) - LeetSync

---
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+void f(TreeNode* root,map<int, vector<pair<int, int>>>&mp,TreeNode* par,int l){
+ if(!root) return;
+ mp[l].push_back({root->val,par->val});
+ f(root->left,mp,root,l+1);
+ f(root->right,mp,root,l+1);
+ return;
+ 
+ 
+}
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        TreeNode* t= new TreeNode(-1);
+     map<int, vector<pair<int,int>>>mp1,mp2;
+     f(root1,mp1,t,0);
+     f(root2,mp2,t,0);
+     for(auto it: mp1){ 
+     sort(mp1[it.first].begin(),mp1[it.first].end());
+     }
+      for(auto it: mp2){ 
+      sort(mp2[it.first].begin(),mp2[it.first].end());
+      }
+      
+     
+     return  mp1==mp2;
+    }
+};
