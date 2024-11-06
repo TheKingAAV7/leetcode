4ebe79bf529d682e7e9da703cf1bc9409bd397ca
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2024-11-05 22:35:45 -0800

    Time: 47 ms (6.48%) | Memory: 38.8 MB (5.05%) - LeetSync

---
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+        unordered_map<int, set<int>>mp;
+        unordered_map<int, int>pos;
+        int setb;
+        for(int i=0;i<nums.size();i++){
+            pos[nums[i]]=i;
+            setb=__popcount(nums[i]);
+            //cout<<nums[i]<<"->"<<setb<<endl;
+            mp[setb].insert(i);
+        } // 2 4 8 15 30
+        // for(auto it: mp){
+        //     cout<<it.first<<"-> ";
+        //     for(auto i: it.second) cout<<i<<" ";
+        //     cout<<endl;
+        // }
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            setb=__popcount(nums[i]);
+            int req= i;
+            int cur= pos[nums[i]];
+         
+            while(true and (cur!=req)){
+             
+             if(cur>req) cur--;
+             else cur++;
+             if(mp[setb].find(cur)==mp[setb].end()) return false;
+            }
+            //    cout<<req<<" "<<cur<<endl;
+        }
+        return true;
+
+       
+    }
+};
