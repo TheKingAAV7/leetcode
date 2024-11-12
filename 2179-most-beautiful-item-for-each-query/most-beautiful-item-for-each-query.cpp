4a2a53b8136dc119563a27df41d870f9d6296c98
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2024-11-11 23:04:21 -0800

    Time: 43 ms (95.90%) | Memory: 93.3 MB (67.57%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -1,44 +1,43 @@
-
-#include<bits/stdc++.h>
 class Solution {
 public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-       map<int, int>mp;
-       for(auto it: items){
-        if(mp.find(it[0])==mp.end()){
-            mp[it[0]]=it[1];
+        sort(items.begin(),items.end());
+        int n = items.size();
+        vector<int> maxBeauty(n),prices(n);
+        maxBeauty[0] = items[0][1];
+        prices[0] = items[0][0];
+        for(int i =1;i<n;i++) {
+            prices[i] = items[i][0];
+            maxBeauty[i] = max(maxBeauty[i-1],items[i][1]);
         }
-        else mp[it[0]] = max(mp[it[0]],it[1]);
-       }
-       vector<int>ans;
-       vector<pair<int, int>>st(mp.begin(),mp.end());
-       int maxi=0;
-       for(auto &it:st){
-        maxi=max(it.second,maxi);
-        it.second=maxi;
-       }
-      // for(auto it: st) cout<<it.first<<" "<<it.second<<endl;
-       for(auto i: queries){
-        auto it= lower_bound(st.begin(),st.end(),make_pair(i,0));
-        if(it==st.begin()){
-            if(it->first<=i){
-                ans.push_back(it->second);
+        int m = queries.size();
+        vector<int> ans(m);
+        for(int i = 0;i<m;i++) {
+            int idx = bs(prices,queries[i]);
+            if(idx == -1) {
+                ans[i] = 0;
             }
-            else ans.push_back(0);
-        }
-        else if(it==st.end()){
-            it--;
-            ans.push_back(it->second);
-        }
-        else{
-            if(it->first<=i)
-            ans.push_back(it->second);
-            else{
-                it--;
-                ans.push_back(it->second);
+            else {
+                ans[i] = maxBeauty[idx];
             }
         }
-       }
-       return ans;
+        return ans;
+    }
+
+    int bs(vector<int>& prices,int target) {
+        int low = 0;
+        int high = prices.size() - 1;
+        int result = -1;
+        while(low <= high) {
+            int mid = low + (high -low) / 2;
+            if(prices[mid] <= target) {
+                result = mid;
+                low = mid +1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return result;
     }
 };
