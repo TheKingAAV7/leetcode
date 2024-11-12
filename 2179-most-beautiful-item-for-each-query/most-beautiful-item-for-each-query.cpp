fe50f6ef6353dbae19e40e7a1e41b03587f5144d
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2024-11-11 23:02:46 -0800

    Time: 114 ms (8.20%) | Memory: 111 MB (10.47%) - LeetSync

---
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -0,0 +1,44 @@
+
+#include<bits/stdc++.h>
+class Solution {
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+       map<int, int>mp;
+       for(auto it: items){
+        if(mp.find(it[0])==mp.end()){
+            mp[it[0]]=it[1];
+        }
+        else mp[it[0]] = max(mp[it[0]],it[1]);
+       }
+       vector<int>ans;
+       vector<pair<int, int>>st(mp.begin(),mp.end());
+       int maxi=0;
+       for(auto &it:st){
+        maxi=max(it.second,maxi);
+        it.second=maxi;
+       }
+      // for(auto it: st) cout<<it.first<<" "<<it.second<<endl;
+       for(auto i: queries){
+        auto it= lower_bound(st.begin(),st.end(),make_pair(i,0));
+        if(it==st.begin()){
+            if(it->first<=i){
+                ans.push_back(it->second);
+            }
+            else ans.push_back(0);
+        }
+        else if(it==st.end()){
+            it--;
+            ans.push_back(it->second);
+        }
+        else{
+            if(it->first<=i)
+            ans.push_back(it->second);
+            else{
+                it--;
+                ans.push_back(it->second);
+            }
+        }
+       }
+       return ans;
+    }
+};
