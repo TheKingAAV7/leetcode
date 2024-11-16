0e4b096caef1627d33326e277d32ac58fed8476f
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2024-11-15 18:16:57 -0800

    Time: 0 ms (100.00%) | Memory: 35.4 MB (8.45%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
@@ -2,20 +2,38 @@ class Solution {
 public:// 1 2 3 4 3 2
     vector<int> resultsArray(vector<int>& nums, int k) {
         vector<int>ans;
-        bool f;
-        int prev;
+        int ind=-1;
         int n= nums.size();
-        for(int i=0;i<n-k+1;i++){
-            f=false;
-            prev=-1;
-            for(int j=i;j<i+k;j++){
-                if(prev==-1 || (nums[j]==prev+1)){
-                    prev=nums[j];
-                }
-                else f=true;
+        deque<pair<int, int>>q;
+        for(int i=0;i<k;i++){
+            if(q.empty() || nums[i]== q.back().first+1){
+                q.push_back({nums[i],i});
             }
-            if(!f) ans.push_back(nums[i+k-1]);
+            else{
+                ind=q.back().second;
+                q.push_back({nums[i],i});
+            }
+        }
+        if(ind==-1){
+            ans.push_back(q.back().first);
+        }
+        else ans.push_back(-1);
+        for(int i=k;i<n;i++){
+            if(q.front().second==ind){
+                ind=-1;
+            }
+            q.pop_front();
+             if(q.empty() || nums[i]== q.back().first+1){
+                q.push_back({nums[i],i});
+            }
+            else{
+                ind=q.back().second;
+                q.push_back({nums[i],i});
+            }
+
+            if(ind==-1) ans.push_back(q.back().first);
             else ans.push_back(-1);
+
         }
         return ans;
     }
