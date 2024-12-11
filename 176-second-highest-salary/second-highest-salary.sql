906b7b855ef06b3723ff1ac602372d76868a16f7
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2024-12-10 23:19:12 -0800

    Time: 257 ms (90.41%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,13 +1,7 @@
+# Write your MySQL query statement below
 
--- select e.salary as SecondHighestSalary from Employee e 
--- where max(e.salary) < (select max(salary) from employee)
-
-select max(x.salary) as SecondHighestSalary from (
-select * from Employee o 
-where  o.salary < (select max(salary) from employee)
-
-) as x
-
-
--- select e.salary as SecondHighestSalary from employee e
--- where e.id = (select id from employee order by salary desc limit 1 offset 1)
+select max(salary) as SecondHighestSalary
+from (
+select * from Employee e
+where e.salary < (select max(f.salary) from Employee f )
+) as st;
