a1bc4a258403b5b0e94f0cc17af9d846da6c1406
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2024-12-26 02:36:22 -0800

    Time: 235 ms (40.14%) | Memory: 56.3 MB (16.64%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,21 +1,28 @@
 class Solution {
 private:
+map<int,map<int,int>>dp;
 int f(int i, int n,vector<int>&nums,int target,int ans){
     if(i==n-1){
         int one=ans+nums[i];
         int two=ans-nums[i];
         return (one==target) + (two==target);
     }
+    if(dp.find(i)!=dp.end() and dp[i].find(ans)!=dp[i].end()){
+        return dp[i][ans];
+    }
 
     int plus= f(i+1,n,nums,target,ans+nums[i]);
     int minus=f(i+1,n,nums,target,ans-nums[i]);
-    return plus+minus;
+    return dp[i][ans]=plus+minus;
 }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int ans=0;
         int n=nums.size();
+      
+     
         int res=f(0,n,nums,target,ans);
+        
         return res;
     }
 };
