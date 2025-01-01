9edca320a4d338caeb08b0ec44ecdac99bf9932d
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2024-12-31 22:40:03 -0800

    Time: 0 ms (100.00%) | Memory: 8.8 MB (38.26%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int n=s.length();
-        vector<int>pref(n,0);
-        int cnt=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='1') cnt++;
-            pref[i]=cnt;
-        }
-        // for(int i: pref) cout<< i<<" ";
-        // cout<<endl;
-        int ans=0;
-        cnt=0;
-       // if(s[0]=='0' ) cnt++;
-        for(int i=0;i<n-1;i++){
+        int ones = count(s.begin(), s.end(), '1');
+        
+        int ans = 0;
+        int zeros = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] == '1') {
+                ones--;
+            } else {
+                zeros++;
+            }
             
-            if(s[i]=='0') cnt++;
-            ans=max(ans,cnt+pref[i+1]);
-            
+            ans = max(ans, zeros + ones);
         }
+        
         return ans;
     }
 };
