bf6e384bcb324a74aaf36d5662bf250897eb159e
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-01-24 22:11:47 +0530

    Time: 0 ms (100.00%) | Memory: 51.9 MB (88.60%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,34 +1,29 @@
 class Solution {
 private:
-bool f(int node,vector<vector<int>>&graph,vector<int>&vis,vector<int>&rec){
-    vis[node]=1;
-    rec[node]=1;
+vector<int>dp;
+bool f(int node, vector<vector<int>>&graph){
+    if(dp[node]!=-1) return dp[node];
+    // assume node as unsafe;
+    dp[node]=0;
     for(auto it:graph[node]){
-        if(vis[it]==0){
-           if( f(it,graph,vis,rec)) return true;
-        }
-        else if(rec[it]==1){
-            return true;
+        if(!f(it,graph)){
+            return false;
         }
     }
-    rec[node]=0;
-    return false;
+    dp[node]=1;
+    return true;
 }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int>vis(n,0),rec(n,0);
-        for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                f(i,graph,vis,rec);
-            }
-        }
+        dp.resize(n,-1);
         vector<int>ans;
         for(int i=0;i<n;i++){
-            if(rec[i]==0){
+            if(f(i,graph)){
                 ans.push_back(i);
             }
         }
         return ans;
+        
     }
 };
