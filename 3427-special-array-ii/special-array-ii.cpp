07a261748e10b3d3184418ea71aef4ca924f45d9
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-02-01 21:34:07 +0530

    Time: 87 ms (8.27%) | Memory: 140.4 MB (5.52%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-        set<int>st;
-        int n=nums.size();
-        for(int i=0;i<n-1;i++){
-          if((nums[i]+nums[i+1])%2==0){
-             st.insert(i);
-          } 
-        }
-        for(int i: st) cout<<i<<" ";
-        cout<<endl;
-        vector<bool>ans;
-        for(auto it1:queries){
-            auto it=st.lower_bound(it1[0]);
-            if(it==st.end()){
-                ans.push_back(true);
-            }
-            else{
-                if(*it < it1[1]) ans.push_back(false);
-                else ans.push_back(true);
-            }
-        }
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& que) {
+       int n=nums.size();
+       set<int>st;
+       vector<bool>ans;
+       if(n==1){
+        int m=que.size();
+        ans.resize(m,true);
         return ans;
+       }
+       for(int i=0;i<n-1;i++){
+        if(nums[i]%2 == nums[i+1]%2){
+            st.insert(i);
+        }
+       }
+       for(auto it:que){
+            int l=it[0],r=it[1];
+            if(l==r){
+                ans.push_back(true);
+                continue;
+            }
+            auto it1=st.lower_bound(l);
+            if(it1==st.end()){
+             ans.push_back(true);
+             continue;
+            }
+            if(*it1<r){
+                ans.push_back(false);
+            }
+            else ans.push_back(true);
+       }
+       return ans;
     }
 };
