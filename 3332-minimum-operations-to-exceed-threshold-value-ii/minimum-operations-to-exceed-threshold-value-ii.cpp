7e8f822e5a4c7422850dd7f08de28a54ec85a573
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-02-12 18:25:31 -0800

    Time: 105 ms (23.61%) | Memory: 99 MB (61.54%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -3,36 +3,26 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
         int n=nums.size();
-        int l=0,h=n;
-        int ans=-1;
-        auto f=[&](int mid)->bool{
-            //cout<<mid<<endl;
-            priority_queue<ll,vector<ll>,greater<ll>>pq;
-            for(ll i:nums)pq.push(i);
-            while(mid and pq.size()>=2){
+       
+       priority_queue<ll,vector<ll>,greater<ll>>pq;
+        for(ll i:nums){
+            pq.push(i);
+        }
+        if(n<2){
+            return pq.top()>=k;
+        }
+        int cnt=0;
+        while(pq.size()>=2){
+            if(pq.top()>=k){
+                break;
+            }
             ll a=pq.top();
             pq.pop();
             ll b=pq.top();
             pq.pop();
             pq.push(min(a,b)*2+max(a,b));
-            mid--;
-            }
-            while(!pq.empty()){
-                if(pq.top()<k) return false;
-                pq.pop();
-            }
-            return true;
-        };
-        while(l<=h){
-            int mid=(l+h)>>1;
-            if(f(mid)){
-                ans=mid;
-                h=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
+            cnt++;
         }
-        return ans;
+        return cnt;
     }
 };
