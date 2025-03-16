69a99f7c78dd796a44639b23232c1af79d7d2019
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-03-16 03:23:22 -0700

    Time: 75 ms (18.83%) | Memory: 59.4 MB (14.69%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -0,0 +1,28 @@
+#define ll long long
+class Solution {
+public:
+    long long repairCars(vector<int>& ranks, int cars) {
+        sort(ranks.begin(),ranks.end());
+        ll lo=0,hi=1e18;
+        ll ans=hi;
+        auto f=[&](ll m)->bool{
+            ll cnt=0;
+            ll tmp=m;
+            for(auto it:ranks){
+                tmp=m;
+                tmp/=it;
+                cnt+=(sqrt(tmp));
+            }
+            return cnt>=cars;
+        };
+        while(lo<=hi){
+            ll mid=lo+(hi-lo)/2;
+            if(f(mid)){
+                ans=mid;
+                hi=mid-1;
+            }
+            else lo=mid+1;
+        }
+        return ans;
+    }
+};
