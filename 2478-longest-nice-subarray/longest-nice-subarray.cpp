e9a355b9d76502df4281245e4bb451e88f0d72c4
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-03-18 06:13:55 -0700

    Time: 142 ms (10.67%) | Memory: 60.8 MB (87.70%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -6,28 +6,38 @@ public:
             return 1;
         vector<int> tmp(32, 0);
         int ans = 1;
+        int maxi=0;
         for (int i = 0; i < 31; i++) {
-            if (1 & (nums[0] >> i))
+            if (1 & (nums[0] >> i)){
                 tmp[i]++;
+                maxi=max(tmp[i],maxi);
+            }
         }
         
         int j = 1;
         int i = 0;
         while (j < n) {
             for (int k = 0; k < 31; k++) {
-                if (1 & (nums[j] >> k))
+                if (1 & (nums[j] >> k)){
                     tmp[k]++;
-            }
-            while (j - i + 1 > 1 and *max_element(tmp.begin(), tmp.end()) > 1) {
-               
-                for (int k = 0; k < 31; k++) {
-                    if (1 & (nums[i] >> k))
-                        tmp[k]--;
+                    
                 }
+                maxi=max(tmp[k],maxi);
+            }
+            while (j - i + 1 > 1 and maxi > 1) {
+                int cur=0;
+                for (int k = 0; k < 31; k++) {
+                    if (1 & (nums[i] >> k)){
+                        tmp[k]--;
+                        
+                    }
+                    cur=max(cur,tmp[k]);
+                }
+                maxi=cur;
                 i++;
             }
          
-            if(*max_element(tmp.begin(),tmp.end())<=1) ans=max(ans,j-i+1);
+            if(maxi<=1) ans=max(ans,j-i+1);
 
             j++;
         }
