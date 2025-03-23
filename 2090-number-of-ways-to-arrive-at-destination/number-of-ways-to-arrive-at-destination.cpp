5bea0f04732dc1a186cecf932b57cd39f947895f
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-03-23 23:27:54 +0530

    Time: 3 ms (91.44%) | Memory: 36.1 MB (67.41%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -2,34 +2,39 @@
 
 class Solution {
 public:
-    int mod = 1e9 + 7;
+    int mod = 1e9+7;
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<ll, ll>>> adj(n);
-        for(auto it: roads){
-            adj[it[0]].push_back({it[1], it[2]});
-            adj[it[1]].push_back({it[0], it[2]});
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto &r : roads){
+            adj[r[0]].push_back({r[1], r[2]});
+            adj[r[1]].push_back({r[0], r[2]});
         }
-        vector<ll> dist(n, LLONG_MAX);
-        vector<ll> ways(n, 0);
-        dist[0] = 0;
-        ways[0] = 1;
-        priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<>> pq;
-        pq.push({0, 0});
+        vector<ll> d(n, LLONG_MAX);
+        priority_queue<pair<ll,int>, vector<pair<ll,int>>, greater<pair<ll,int>>> pq;
+        d[n-1] = 0;
+        pq.push({0, n-1});
         while(!pq.empty()){
-            auto [d, u] = pq.top();
+            auto [dist, u] = pq.top();
             pq.pop();
-            if(d > dist[u])
-                continue;
-            for(auto &[v, w]: adj[u]){
-                if(dist[u] + w < dist[v]){
-                    dist[v] = dist[u] + w;
-                    ways[v] = ways[u];
-                    pq.push({dist[v], v});
-                } else if(dist[u] + w == dist[v]){
-                    ways[v] = (ways[v] + ways[u]) % mod;
+            if(dist > d[u]) continue;
+            for(auto &[v, w] : adj[u]){
+                if(dist + w < d[v]){
+                    d[v] = dist + w;
+                    pq.push({d[v], v});
                 }
             }
         }
-        return ways[n-1] % mod;
+        vector<int> dp(n, -1);
+        function<int(int)> dfs = [&](int u) -> int {
+            if(u == n-1) return 1;
+            if(dp[u] != -1) return dp[u];
+            ll ways = 0;
+            for(auto &[v, w] : adj[u]){
+                if(d[u] == d[v] + w)
+                    ways = (ways + dfs(v)) % mod;
+            }
+            return dp[u] = ways;
+        };
+        return dfs(0);
     }
 };
