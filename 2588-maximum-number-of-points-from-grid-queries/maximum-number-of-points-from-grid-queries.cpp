e0cb6862bfb211c09659fc9e1e2b386d741d7b15
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-03-28 18:27:59 +0530

    Time: 150 ms (33.70%) | Memory: 44.3 MB (43.84%) - LeetSync

--- 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
@@ -1,55 +1,38 @@
 class Solution {
 public:
-    vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
-        int m = grid.size(), n = grid[0].size();
-        int k = queries.size();
-        
-        // Store original indices before sorting queries
-        vector<int> pos(k);
-        iota(pos.begin(), pos.end(), 0);
-        sort(pos.begin(), pos.end(), [&](int a, int b) {
-            return queries[a] < queries[b]; // Sort queries in ascending order
-        });
-
-        // Direction vectors for 4-way movement
-        vector<int> row = {0, 0, 1, -1};
-        vector<int> col = {1, -1, 0, 0};
-
-        // Min-Heap (value, x, y)
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
-        pq.push({grid[0][0], {0, 0}});
-
-        vector<vector<bool>> vis(m, vector<bool>(n, false));
-        vis[0][0] = true;
-
-        vector<int> ans(k, 0);
-        int count = 0; // Number of nodes visited
-        int prevValue = 0; // Last query value processed
-
-        for (int i = 0; i < k; i++) {
-            int q = queries[pos[i]];
-
-            // Expand the min-heap while nodes have values < current query
-            while (!pq.empty() && pq.top().first < q) {
-                auto [val, cell] = pq.top();
-                pq.pop();
-                int x = cell.first, y = cell.second;
-
-                count++; // New node visited
-
-                // Expand in 4 directions
-                for (int d = 0; d < 4; d++) {
-                    int nx = x + row[d], ny = y + col[d];
-                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && !vis[nx][ny]) {
-                        vis[nx][ny] = true;
-                        pq.push({grid[nx][ny], {nx, ny}});
-                    }
-                }
+constexpr int static n = 1000000;
+int bt[n + 1] = {};
+int prefix_sum(int i)
+{
+    int sum = 0;
+    for (i = i + 1; i > 0; i -= i & (-i))
+        sum += bt[i];
+    return sum;
+}
+void add(int i, int val)
+{
+    for (i = i + 1; i <= n; i += i & (-i))
+        bt[i] += val;
+}
+int dirs[5] = { -1, 0, 1, 0, -1};
+vector<int> maxPoints(vector<vector<int>>& g, vector<int>& qs) {
+    int si = g.size(), sj = g[0].size(), cur = 1;
+    priority_queue<array<int, 3>> pq;
+    pq.push({-g[0][0], 0, 0});
+    g[0][0] = 0;
+    while(!pq.empty()) {
+        auto [nval, i, j] = pq.top(); pq.pop();
+        cur = max(cur, -nval);
+        add(cur, 1);
+        for (int d = 0; d < 4; ++d) {
+            int x = i + dirs[d], y = j + dirs[d + 1];
+            if (min(x, y) >= 0 && x < si && y < sj && g[x][y]) {
+                pq.push({-g[x][y], x, y});
+                g[x][y] = 0;
             }
-
-            ans[pos[i]] = count;
         }
-
-        return ans;
+    }
+    transform(begin(qs), end(qs), begin(qs), [&](int val){ return prefix_sum(val - 1); });
+    return qs;
     }
 };
