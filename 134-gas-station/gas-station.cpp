f5e5f1f928770f7f73e21a87081d49afe6470dd2
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-03-30 17:16:21 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (13.98%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int start = 0;
-        int totalGas = 0;
-        int currentGas = 0;
-
-        for (int i = 0; i < gas.size(); i++) {
-            int fuelGain = gas[i] - cost[i];
-            totalGas += fuelGain;
-            currentGas += fuelGain;
-
-            if (currentGas < 0) {
-                currentGas = 0;
-                start = i + 1;
+      int req=0;
+        int extra=0;
+        int cur=0;
+        int n=gas.size();
+        int idx=-1;
+        for(int i=0;i<n;i++){
+            cur+=gas[i];
+            if(cur<cost[i]){
+                req+=(cost[i]-cur);
+                cur=0;
+                idx=-1;
+                
+            }
+            else{
+                if(idx==-1){
+                idx=i;
+                }
+                extra=(cur-cost[i]);
+                cur-=cost[i];
             }
         }
-
-        return totalGas < 0 ? -1 : start;        
+      //  cout<<req<<" "<<extra<<endl;
+        
+        return extra>=req?idx:-1;     
     }
 };
