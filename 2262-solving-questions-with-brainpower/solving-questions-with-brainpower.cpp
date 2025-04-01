a27c3ab3b304446f4d28eab8f6e97aca145c6cce
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-04-01 11:36:30 +0530

    Time: 10 ms (35.89%) | Memory: 119.6 MB (94.99%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -3,15 +3,13 @@
 class Solution {
 public:
     long long mostPoints(vector<vector<int>>& que) {
-        int n=que.size();
-        vector<ll>dp(n+1,0);
-        for(int i=n-1;i>=0;i--){
-            dp[i]=max(dp[i],dp[i+1]);
-            dp[i]=max(dp[i],que[i][0]+dp[min(i+que[i][1]+1,n)]);
+        int n = que.size();
+        vector<ll> dp(n + 1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = max(dp[i], dp[i + 1]);
+            dp[i] = max(dp[i], que[i][0] + dp[min(i + que[i][1] + 1, n)]);
         }
-       
-        
+
         return dp[0];
-        
     }
 };
