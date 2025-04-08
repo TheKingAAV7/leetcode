abcc74749a15d8b7018c6bd48ceb9e789df74aa5
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-04-08 11:04:42 +0530

    Time: 3 ms (55.97%) | Memory: 28.5 MB (29.69%) - LeetSync

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        map<int,int>mp;
+        int n=nums.size();
+        int maxi=-1;
+        for(int i=0;i<n;i++){
+            if(mp.find(nums[i])!=mp.end()){
+               
+                maxi=max(mp[nums[i]],maxi);
+            }
+            mp[nums[i]]=i;
+        }
+        cout<<maxi<<endl;
+
+        return ceil((1.0*(maxi+1))/3.0);
+    }
+};
