de5cafc6216b8ed9e9c92d686aa86ea9d353a41f
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-04-20 10:09:24 +0530

    Time: 2 ms (28.54%) | Memory: 12.3 MB (42.46%) - LeetSync

---
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& arr) {
+        int n=arr.size();
+        map<int,int>mp;
+        int ans=0;
+        for(int i:arr) mp[i]++;
+        for(auto it:mp){
+            double x=it.first;
+            double cnt=ceil(it.second/(x+1.00));
+            ans+=(ceil(cnt)*(x+1));
+        }
+        return ans;
+    }
+};
