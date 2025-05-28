6cacdd0f8a6fab38eb8252f8d41ed80f4f3e0ebf
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-05-28 21:36:10 +0530

    Time: 0 ms (100.00%) | Memory: 60.3 MB (62.56%) - LeetSync

--- 2437-maximum-number-of-groups-entering-a-competition/maximum-number-of-groups-entering-a-competition.cpp
+++ 2437-maximum-number-of-groups-entering-a-competition/maximum-number-of-groups-entering-a-competition.cpp
@@ -1,31 +1,6 @@
 class Solution {
 public:
-    int maximumGroups(vector<int>& grades) {
-       // sum[i]<sum[i+1]
-       // no[i]<no[i+1]    
-       int ans=1;
-       sort(grades.begin(),grades.end());
-       int n=grades.size();
-
-        int prev=grades[0];
-        int cnt=1;
-        int gps=1; // 3 5 6 7 10 12
-        int sm=0;
-        int cur_cnt=0;
-        for(int i=1;i<n;i++){
-            sm+=grades[i];
-            cur_cnt++;
-            if(sm>prev and cur_cnt>cnt){
-              //  cout<<i<<endl;
-                gps++;
-                cnt=cur_cnt;
-                prev=sm;
-                cur_cnt=0;
-                sm=0;
-            }
-        }
-       // if(sm>prev and cur_cnt>cnt) gps++;
-       
-       return gps;
+    int maximumGroups(vector<int>& A) {
+      return (int)(sqrt(A.size() * 2 + 0.25) - 0.5);
     }
 };
