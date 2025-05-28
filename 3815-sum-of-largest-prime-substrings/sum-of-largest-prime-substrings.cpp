9e71c352ce63ae033384c238b588d2bccd64c33e
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-05-28 15:05:22 +0530

    Time: 11 ms (72.01%) | Memory: 9.9 MB (92.85%) - LeetSync

---
+++ 3815-sum-of-largest-prime-substrings/sum-of-largest-prime-substrings.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    long long sumOfLargestPrimes(string s) {
+        set<long long>st;
+        auto f=[&](long long no)->bool{
+            if(no==1) return false;
+            if(no==2) return true;
+            for(long long i=2;i*i<=no;i++){
+                if(no%i==0) return false;
+            }
+            return true;
+        };
+        int n=s.length();
+        for(int i=0;i<n;i++){
+           long long no=0;
+            for(int j=i;j<n;j++){
+                //tmp.push_back(s[i]-'0');
+               no=no*10+(s[j]-'0');
+                if(f(no)){
+               //     cout<<no<<endl;
+                    st.insert(no);
+                }
+            }
+        }
+        int k=3;
+        long long sm=0;
+        while(!st.empty() and k--){
+            auto it=st.end();
+            it--;
+         
+            sm+=(*it);
+             st.erase(it);
+        }
+        return sm;
+    }
+};
