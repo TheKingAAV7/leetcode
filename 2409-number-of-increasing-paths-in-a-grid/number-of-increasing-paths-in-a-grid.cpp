92e2abf87e47d95e5f9bfc11c87f8fc8d231a7e8
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-05-29 20:27:11 +0530

    Time: 244 ms (18.41%) | Memory: 127.7 MB (11.50%) - LeetSync

--- 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.cpp
+++ 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.cpp
@@ -1,70 +1,69 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 private:
-    vector<int> row = {-1, 1, 0, 0};
-    vector<int> col = {0, 0, -1, 1};
+  int mod=1e9+7;
+vector<int>row={-1,1,0,0};
+vector<int>col={0,0,-1,1};
+void dfs(int i, int j,vector<vector<vector<int>>>&dp,vector<vector<int>>&vis,vector<vector<int>>&deg,vector<vector<int>>&grid,int n, int m){
+    if(vis[i][j]) return;
+   
+    vis[i][j]=1;
 
-    void dfs(int i, int j,
-             vector<vector<vector<int>>>& dp,
-             vector<vector<int>>& vis,
-             vector<vector<int>>& grid,
-             int n, int m) {
-        if (vis[i][j]) return;
-        vis[i][j] = 1;
-
-        for (int k = 0; k < 4; ++k) {
-            int ni = i + row[k], nj = j + col[k];
-            if (ni >= 0 && ni < n && nj >= 0 && nj < m
-                && grid[ni][nj] > grid[i][j]) {
-                if (!vis[ni][nj]) {
-                    dfs(ni, nj, dp, vis, grid, n, m);
-                }
-                // paths from (i,j) going in direction k
-                dp[i][j][k] = (dp[ni][nj][4] + 1) % 1000000007;
+    for(int k=0;k<4;k++){
+        int newx=i+row[k];
+        int newy=j+col[k];
+        
+        if(newx>=0 and newx<n and newy>=0 and newy<m and   grid[i][j]<grid[newx][newy]){
+            
+        
+            deg[newx][newy]++;
+            if(vis[newx][newy]==0){
+                
+                dfs(newx,newy,dp,vis,deg,grid,n,m);
             }
+
+            int a=dp[newx][newy][k];
+            
+            
+            dp[i][j][k]=dp[newx][newy][4]+1;
+            // if(grid[i][j]==1  and i==0 and j==0){
+            
+            
+            // }
         }
 
-        // total paths of length ≥ 2 from (i,j)
-        int total = 0;
-        for (int k = 0; k < 4; ++k) {
-            total = (total + dp[i][j][k]) % 1000000007;
-        }
-        dp[i][j][4] = total;
     }
+    dp[i][j][4]=(dp[i][j][0]+dp[i][j][1]+dp[i][j][2]+dp[i][j][3])%mod;
+  //  cout<<i<<" "<<j<<" -> "<<dp[i][j][4]<<endl;
+    
+    return;
 
+}
 public:
     int countPaths(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        const int MOD = 1000000007;
-
-        // dp[i][j][0..3]: number of inc. paths from (i,j) going in each dir (length≥2)
-        // dp[i][j][4]: sum of dp[i][j][0..3]
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(5, 0)));
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
-        // fill dp via DFS
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (!vis[i][j]) {
-                    dfs(i, j, dp, vis, grid, n, m);
+        int n=grid.size(),m=grid[0].size();
+       vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(5,0)));
+       vector<vector<int>>vis(n,vector<int>(m,0));
+       vector<vector<int>>deg(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j]){
+                    dfs(i,j,dp,vis,deg,grid,n,m);
                 }
             }
         }
-
-        // sum over all cells, adding the single‐cell path
-        long long ans = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                // include the length-1 path [grid[i][j]]
-                dp[i][j][4] = (dp[i][j][4] + 1) % MOD;
-                ans = (ans + dp[i][j][4]) % MOD;
+      //  cout<<endl;
+      
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                dp[i][j][4] = (dp[i][j][4] + 1) % mod;  
+       
+        ans = (ans + dp[i][j][4]) % mod;
             }
+           // cout<<endl;
+           
         }
-
-        return (int)ans;
+        return ans;
     }
 };
-
-
