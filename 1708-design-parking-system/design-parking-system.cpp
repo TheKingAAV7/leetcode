c82f95a44080e4dd1776c2b023b77fcf7530bfe2
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-05-30 19:19:56 +0530

    Time: 10 ms (29.10%) | Memory: 38.7 MB (86.41%) - LeetSync

--- 1708-design-parking-system/design-parking-system.cpp
+++ 1708-design-parking-system/design-parking-system.cpp
@@ -1,33 +1,18 @@
 class ParkingSystem {
 public:
-    int b,m,s;
+    int spot[4];
     ParkingSystem(int big, int medium, int small) {
-        b=big;
-        m=medium;
-        s=small;
+        spot[1]=big;
+        spot[2]=medium;
+        spot[3]=small;
     }
     
     bool addCar(int carType) {
-       if(carType==1){
-        if(b){
-            b--;
+        if(spot[carType]>0){
+            spot[carType]--;
             return true;
         }
         return false;
-        
-       }
-       if(carType==2){
-        if(m){
-            m--;
-            return true;
-        }
-        return false;
-       }
-       if(s){
-        s--;
-        return true;
-       }
-       return false;
     }
 };
 
