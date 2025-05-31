1b9b602abbffdf50d114525d10f4ca40201edb81
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-05-31 15:24:08 +0530

    Time: 84 ms (5.01%) | Memory: 108.1 MB (5.59%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
@@ -1,22 +1,38 @@
+
+#define ll long long
 class Solution {
-public:
+public://2 3 7
     long long countSubarrays(vector<int>& nums, long long k) {
-        long long n=nums.size();
-        long long total=n*(n+1)/2;
-        long long sm=0;
-        long long tmp=0;
-        long long cnt=0;
-        long long l=0;
+        ll ans=0;
+        int n=nums.size();
+        vector<ll>pref(n,0);
+        ll sm=0;
         for(int i=0;i<n;i++){
-            sm+=nums[i];
-            cnt++;
-            while((sm*cnt)>=k){
-                tmp+=(n-i);
-                cnt--;
-                sm-=nums[l];
-                l++;
-            }
+         sm+=nums[i];
+         pref[i]=sm;
         }
-        return total-tmp;
+        auto f=[&](int mid,int i)->bool{
+           
+            ll sm=pref[mid]-pref[i]+nums[i];
+            ll tsm=(sm*(mid-i+1));
+             
+            if(tsm<k) return true;
+            return false;
+        };
+        for(int i=0;i<n;i++){
+            ll tmp=-1;
+            int lo=i,hi=n-1;
+            while(lo<=hi){
+                int mid=(lo+hi)>>1;
+                if(f(mid,i)){
+                    tmp=mid;
+                    lo=mid+1;
+                }
+                else hi=mid-1;
+            }
+            if(tmp!=-1) ans+=(tmp-i+1);
+         //   cout<<i<<" "<<tmp<<endl;
+        }
+        return ans;
     }
 };
