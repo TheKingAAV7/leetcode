782e0e6c14948582c3cd49f788367ec527f96267
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-06-02 15:03:42 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (43.50%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        int n=ratings.size();
-        
-        vector<int>ans(n,1);
-        map<int,vector<int>>mp;
-        for(int i=0;i<n;i++){
-            mp[ratings[i]].push_back(i);
-        }
-        int maxi=*max_element(ratings.begin(),ratings.end());
-        for(int i=1;i<=maxi;i++){
-            if(mp.find(i)!=mp.end()){
-            for(int pos:mp[i]){
-                int maxi=0;
-                if(pos-1 >=0 and ratings[pos]> ratings[pos-1]){
-                    maxi=max(maxi,ans[pos-1]);
-                }
-                if(pos+1 <n and ratings[pos]>ratings[pos+1]) {
-                      maxi=max(maxi,ans[pos+1]);
-                }
-                ans[pos]=maxi+1;
-              }
-            }
-            
-        }
-        int cnt=accumulate(ans.begin(),ans.end(),0);
-        return cnt;
+    int candy(vector<int>& arr) {
+       int n=arr.size();
+       if(n==1) return 1;
+       vector<int>ans(n,0);
+       int cnt=0;
+       for(int i=1;i<n;i++){
+        if(arr[i]>arr[i-1])cnt++;
+        else cnt=0;
+        ans[i]=max(ans[i],cnt);
+       }
+       cnt=0;
+       for(int i=n-2;i>=0;i--){
+            if(arr[i]>arr[i+1]) cnt++;
+            else cnt=0;
+            ans[i]=max(ans[i],cnt);
+       } 
+       int res=accumulate(ans.begin(),ans.end(),n);
+       return res;
     }
 };
