b2799d895aa5237e5b716cec077cd5f8293ad5f0
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-06-07 09:38:04 +0530

    Time: 263 ms (23.70%) | Memory: 120.2 MB (10.39%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -3,19 +3,14 @@ public:
     string clearStars(string s) {
         int n=s.length();
         vector<int>vis(n,0);
-        vector<vector<int>>v(26);
+        map<char,vector<int>>mp;
         for(int i=0;i<n;i++){
             if(s[i]!='*')
-            v[s[i]-'a'].push_back(i);
+            mp[s[i]].push_back(i);
             else{
-                for(int j=0;j<26;j++){
-                    if(v[j].size()!=0){
-                        vis[v[j].back()]=1;
-                        v[j].pop_back();
-                        break;
-                    }
-                    
-                }
+                vis[mp.begin()->second.back()]=1;
+                mp.begin()->second.pop_back();
+                if(mp.begin()->second.size()==0) mp.erase(mp.begin()->first);
                 vis[i]=1;
             }
         }
