fd85e0101b1971c2c4c7c5649f7b78c25abbdb86
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-06-18 16:50:43 +0530

    Time: 38 ms (31.60%) | Memory: 77.9 MB (22.51%) - LeetSync

---
+++ 1223-graph-connectivity-with-threshold/graph-connectivity-with-threshold.cpp
@@ -0,0 +1,59 @@
+class Solution {
+private:
+vector<int>rank,parent;
+int find(int node){
+    if(parent[node]==node) return node;
+    return parent[node]=find(parent[node]);
+}
+bool unif(int x, int y){
+    int parx=find(x);
+    int pary=find(y);
+    if(parx==pary) return true;
+    return false;
+}
+
+void uni(int x, int y ){
+    int parx=find(x);
+    int pary=find(y);
+    if(parx==pary) return;
+    if(rank[parx]>rank[pary]){
+        parent[pary]=parx;
+    }
+    else if(rank[parx]==rank[pary]){
+        parent[pary]=parx;
+        rank[parx]++;
+    }
+    else{
+        parent[parx]=pary;
+    }
+    return;
+}
+public:
+    vector<bool> areConnected(int n, int threshold, vector<vector<int>>& queries) {
+        parent.resize(n+1);
+        rank.resize(n+1);
+        vector<bool>ans;
+        iota(parent.begin(),parent.end(),0);
+        fill(rank.begin(),rank.end(),0);
+        for (int d = threshold + 1; d <= n; ++d) {
+        for (int m = 2*d; m <= n; m += d) {
+            uni(d, m);
+        }
+    }
+        for(auto it:queries){
+            int g=gcd(it[0],it[1]);
+            if(g>threshold){
+                uni(it[0],it[1]);
+                ans.push_back(true);
+                continue;
+            }
+            if(unif(it[0],it[1])){
+                ans.push_back(true);
+                continue;
+            }
+            ans.push_back(false);
+
+        }
+        return ans;
+    }
+};
