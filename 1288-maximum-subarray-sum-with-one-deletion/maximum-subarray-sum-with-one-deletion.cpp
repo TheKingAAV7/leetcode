1f257416d817d6cb02e0dd8260c7b99c7ef69481
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-06-22 23:16:47 +0530

    Time: 1 ms (44.81%) | Memory: 28.8 MB (31.49%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -2,20 +2,33 @@ class Solution {
 public:
     int maximumSum(vector<int>& arr) {
         int n=arr.size();
-        vector<vector<int>>dp(n,vector<int>(2,0));
-        if(arr[0]>0){
-            dp[0][0]=arr[0];
-            dp[0][1]=arr[0];
-        }
-        for(int i=1;i<n;i++){
-            dp[i][0]=max({dp[i][0],dp[i-1][0]+arr[i],arr[i]});
-            dp[i][1]=max({dp[i][1],dp[i-1][0],dp[i-1][1]+arr[i]});
-        }
-        int maxi=0;
+        if(n==1) return arr[0];
+        vector<int>lef(n,0),rig(n,0);
+        int sm1=0;
+        int mx1=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<2;j++) maxi=max(dp[i][j],maxi);
+            sm1+=arr[i];
+            if(sm1<0) sm1=0;   
+            rig[i]=sm1;
         }
-        if(maxi==0) return *max_element(arr.begin(),arr.end());
-        return maxi;
+        sm1=0,mx1=0;
+        for(int i=n-1;i>=0;i--){
+            sm1+=arr[i];
+            if(sm1<0) sm1=0;
+            lef[i]=sm1;
+        }
+        // for(int i:rig) cout<<i<<" ";
+        // cout<<endl;
+        // for(int i:lef) cout<<i<<" ";
+        // cout<<endl;
+
+        int ans=max({lef[0],lef[1],rig[n-1],rig[n-2]});
+        for(int i=1;i<n-1;i++){
+          
+            ans=max({rig[i-1]+lef[i+1]+arr[i],rig[i-1]+lef[i+1],ans});
+        }
+        if(ans==0) return *max_element(arr.begin(),arr.end());
+        return ans;
+
     }
 };
