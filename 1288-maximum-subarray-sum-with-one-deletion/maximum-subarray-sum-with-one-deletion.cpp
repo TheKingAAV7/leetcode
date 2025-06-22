ac3aac9fd38af4f68d535e0739b8f6eebbb37b34
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-06-22 22:53:07 +0530

    Time: 31 ms (11.25%) | Memory: 38.5 MB (9.17%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -4,25 +4,17 @@ public:
         int n=arr.size();
         vector<vector<int>>dp(n,vector<int>(2,0));
         if(arr[0]>0){
-            dp[0][0]=dp[0][1]=arr[0];
+            dp[0][0]=arr[0];
+            dp[0][1]=arr[0];
         }
-        
         for(int i=1;i<n;i++){
-            
-             dp[i][0]=max({dp[i-1][0]+arr[i],
-                           arr[i],dp[i][0]});
-            
-            //   if(arr[i]<0)
-                dp[i][1]=max({dp[i-1][0],dp[i-1][1]+arr[i],arr[i],0});
-            
+            dp[i][0]=max({dp[i][0],dp[i-1][0]+arr[i],arr[i]});
+            dp[i][1]=max({dp[i][1],dp[i-1][0],dp[i-1][1]+arr[i]});
         }
         int maxi=0;
         for(int i=0;i<n;i++){
-           maxi=max({maxi,dp[i][0],dp[i][1]});
-           //cout<<max(dp[i][0],dp[i][1])<<" ";
+            for(int j=0;j<2;j++) maxi=max(dp[i][j],maxi);
         }
-        
-
         if(maxi==0) return *max_element(arr.begin(),arr.end());
         return maxi;
     }
