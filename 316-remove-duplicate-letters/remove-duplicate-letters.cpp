2ca2e574c60355e6018578e218a0f9f768f98ce7
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-06-22 22:59:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (23.35%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        // bcabc
-        stack<pair<char,int>>st;
         vector<int>mp(26,0);
-        for(char c:s) mp[c-'a']++;
+        for(auto c:s) mp[c-'a']++;
         int n=s.length();
+        stack<char>st;
         set<char>vis;
         for(int i=0;i<n;i++){
             
             if(vis.find(s[i])!=vis.end()){
-             mp[s[i]-'a']--;
-             continue;
+                mp[s[i]-'a']--;
+                continue;
             }
-            while(!st.empty() and (s[i]<st.top().first and mp[st.top().first-'a']>1)){
-                mp[st.top().first-'a']--;
-                vis.erase(st.top().first);
+            while(!st.empty() and (s[i]<st.top()) and mp[st.top()-'a']>1 ){
+                mp[st.top()-'a']--;
+                vis.erase(st.top());
                 st.pop();
             }
-            if(vis.find(s[i])==vis.end()){
             vis.insert(s[i]);
-            st.push({s[i],i});
-            }
-
+            st.push(s[i]);
         }
         string ans="";
         while(!st.empty()){
-            ans.push_back(st.top().first);
-            st.pop();
+            ans.push_back(st.top());st.pop();
         }
         reverse(ans.begin(),ans.end());
-        
         return ans;
     }
 };
