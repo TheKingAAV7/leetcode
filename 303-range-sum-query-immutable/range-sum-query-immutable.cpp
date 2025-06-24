74f6a0fb92ae638c80ad0e2f5295a10c3c056a19
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-06-24 12:42:23 +0530

    Time: 2 ms (54.72%) | Memory: 24.3 MB (23.84%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -3,31 +3,30 @@ private:
     vector<int> segtree;
     vector<int>nums;
     int n;
-    int bt(int idx, int lef, int rig, vector<int>& nums) {
-        if (lef == rig) {
-            segtree[idx] = nums[lef];
-            return segtree[idx];
-        }
-        int mid = (lef + rig) / 2;
-        int left = bt(2 * idx + 1, lef, mid, nums);
-        int right = bt(2 * idx + 2, mid + 1, rig, nums);
-        segtree[idx] = left + right;
-        return segtree[idx];
+   int bt(int i, int lef, int rig ){
+    if(lef==rig){
+        segtree[i]=nums[lef];
+        return segtree[i];
     }
 
-    int query(int idx, int lef, int rig, vector<int>& nums, int l, int r) {
-        if (lef == l and rig == r) {
-            return segtree[idx];
+    int mid=(lef+rig)/2;
+    int left=bt(2*i+1,lef,mid);
+    int right=bt(2*i+2,mid+1,rig);
+    segtree[i]=left+right;
+    return segtree[i];
+   }
+
+    int query(int i, int lef, int rig, int l, int r ){
+        if(lef==l and rig==r) return segtree[i];
+        int mid=(lef+rig)/2;
+        if(r<=mid){
+            return query(2*i+1,lef, mid, l, r);
         }
-        int mid = (lef + rig) / 2;
-        // three cases
-        if (lef <= l and r <= mid) {
-            return query(2 * idx + 1, lef, mid, nums, l, r);
-        } else if ((mid + 1) <= l and r <= rig) {
-            return query(2 * idx + 2, mid + 1, rig, nums, l, r);
+        else if((mid+1)<=l){
+            return query(2*i+2,mid+1,rig,l,r);
         }
-        int left = query(2 * idx + 1, lef, mid,nums,l,mid);
-        int right= query(2 * idx + 2, mid + 1, rig,nums, mid+1,r);
+        int left= query(2*i+1,lef, mid, l,mid);
+        int right=query(2*i+2,mid+1, rig,mid+1, r);
         return left+right;
     }
 
@@ -36,11 +35,11 @@ public:
          n = nums1.size();
          nums=nums1;
         segtree.resize(4 * n, 0);
-        bt(0, 0, n - 1, nums);
+        bt(0, 0, n - 1);
     }
 
     int sumRange(int left, int right) {
-        return query(0,0,n-1,nums,left,right);
+        return query(0,0,n-1,left,right);
     }
 };
 
