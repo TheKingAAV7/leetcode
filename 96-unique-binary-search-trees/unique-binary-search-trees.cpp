9acdfd3d5c2cb95286b7fd5bb56233490aa0191c
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-06-26 16:43:29 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (39.21%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -3,16 +3,11 @@ public:
     int numTrees(int n) {
         vector<int>dp(n+1,0);
         dp[1]=1;
+        dp[0]=1;
         for(int i=2;i<=n;i++){
             int sm=0;
             for(int j=1;j<=i;j++){
-                int lef=(j-1);
-                int rig=(i-j);
-                if(lef==0) sm+=dp[rig];
-                else if(rig==0) sm+=dp[lef];
-                else{
-                    sm+=(dp[lef]*dp[rig]);
-                }
+               sm+=(dp[j-1]*dp[i-j]);
             }
             dp[i]=sm;
         }
