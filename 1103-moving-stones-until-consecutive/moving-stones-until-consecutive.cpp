5d6d4b10c7e3cb3706fb0cffcd5da425b4d8773c
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-06-28 19:40:17 +0530

    Time: 594 ms (4.31%) | Memory: 373.4 MB (5.49%) - LeetSync

---
+++ 1103-moving-stones-until-consecutive/moving-stones-until-consecutive.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+int f(int x, int y, int z,vector<vector<vector<int>>>&dp){
+    if(x+1==y and y+1==z) return 0;
+    if(dp[x][y][z]!=-1) return dp[x][y][z];
+    int ans=0;
+    for(int i=x+1;i<y;i++){
+        ans=max(1+f(i,y,z,dp),ans);
+    }
+    for(int j=y+1;j<z;j++){
+        ans=max(1+f(y,j,z,dp),ans);
+    }
+    for(int i=y+1;i<z;i++){
+        ans=max(1+f(x,y,i,dp),ans);
+    }
+    for(int j=x+1;j<y;j++){
+        ans=max(1+f(x,j,y,dp),ans);
+    }
+    return dp[x][y][z]=ans;
+}
+public:
+    vector<int> numMovesStones(int a, int b, int c) {
+       
+        vector<int>tmp={a,b,c};
+        sort(tmp.begin(),tmp.end());
+        int x=tmp[0],y=tmp[1],z=tmp[2];
+        vector<vector<vector<int>>>dp(105,vector<vector<int>>(105,vector<int>(105,-1)));
+       
+        int maxi=f(x,y,z,dp);
+          if(x+1==y and y+1==z) return {0,maxi};
+        int mini=(x+1==y || y+1==z || (y-x==2 || z-y==2))?1:2;
+        return {mini,maxi};
+    }
+};
