ec1e57876564a12bef375d694719fb688a136340
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-06-28 15:18:03 +0530

    Time: 27 ms (70.51%) | Memory: 44.3 MB (22.08%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,19 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll  unsigned long long
 
 class Solution {
 public:
-    int numDistinct(const string &s, const string &t) {
+    int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
-        typedef unsigned long long ull;
-        vector<vector<ull>> dp(n+1, vector<ull>(m+1, 0));
-        for (int i = 0; i <= n; i++) 
-            dp[i][0] = 1;
+        vector<vector<ll>> dp(n+1, vector<ll>(m+1, 0));
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 dp[i][j] = dp[i-1][j];
-                if (s[i-1] == t[j-1]) 
-                    dp[i][j] += dp[i-1][j-1];
+                if (s[i-1] == t[j-1]) dp[i][j] += dp[i-1][j-1];
             }
         }
         return (int)dp[n][m];
