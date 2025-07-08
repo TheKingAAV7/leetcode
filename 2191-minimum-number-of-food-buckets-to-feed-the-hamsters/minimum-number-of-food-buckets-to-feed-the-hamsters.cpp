b459b3652094d2d54e5ed15b56318a6846e52922
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-07-08 23:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (67.28%) - LeetSync

--- 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.cpp
+++ 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.cpp
@@ -1,27 +1,29 @@
 class Solution {
-public:   
+public:
     int minimumBuckets(string ham) {
-        int n=ham.size();
-     
-        int cnt=0;
-        vector<int>ind;
-        for(int i=0;i<n;i++){
-            if(ham[i]=='H') ind.push_back(i);
-        }
-        for(auto idx:ind){
-            if(idx-1>=0 and ham[idx-1]=='@') continue;
-            if(idx<n and ham[idx+1]=='@') continue;
-            if(idx+1<n and ham[idx+1]=='.'){
-                ham[idx+1]='@';
-                cnt++;
+        int n = ham.size();
+
+        int cnt = 0;
+        vector<int> ind;
+        for (int i = 0; i < n; i++) {
+            if (ham[i] == 'H') {
+                int idx = i;
+                if (idx - 1 >= 0 and ham[idx - 1] == '@')
+                    continue;
+                if (idx < n and ham[idx + 1] == '@')
+                    continue;
+                if (idx + 1 < n and ham[idx + 1] == '.') {
+                    ham[idx + 1] = '@';
+                    cnt++;
+                } else if (idx - 1 >= 0 and ham[idx - 1] == '.') {
+                    ham[idx - 1] = '@';
+                    cnt++;
+                } else
+                    return -1;
             }
-            else if(idx-1 >=0 and ham[idx-1]=='.'){
-                ham[idx-1]='@';
-                cnt++;
-            }
-            else return -1;
         }
-       
+        
+
         return cnt;
     }
 };
