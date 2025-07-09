2e8921620ed194921604a9977be214b90c9b4154
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-07-09 18:00:21 +0530

    Time: 20 ms (14.05%) | Memory: 143.4 MB (5.24%) - LeetSync

--- 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
     int maxFreeTime(int eventTime, int k, vector<int>& st, vector<int>& et) {
-        int n=st.size();
-        vector<int>gaps(n+1);
-        gaps[0]=st[0];
-        gaps[n]=eventTime-et[n-1];
-        for(int i=1;i<n;i++){
-            gaps[i]=st[i]-et[i-1];
+     vector<pair<int,int>>tmp;
+     vector<pair<char,int>>v;
+     int n=st.size();
+     for(int i=0;i<n;i++) tmp.push_back({st[i],et[i]});
+     if(tmp[0].first>0){
+        v.push_back({'G',tmp[0].first});
+     }
+     v.push_back({'E',tmp[0].second-tmp[0].first});
+     for(int i=1;i<tmp.size();i++){
+        int pet=tmp[i-1].second;
+        if(pet<tmp[i].first){
+            v.push_back({'G',tmp[i].first-pet});
+            v.push_back({'E',tmp[i].second-tmp[i].first});
         }
-        vector<int>pref(n+2,0);
-        for(int i=1;i<= n+1;i++){
-            pref[i]=pref[i-1]+gaps[i-1];
-        }
-        int ans=0;
-        for(int i=k+1;i<= n+1;i++){
-            ans=max(ans,pref[i]-pref[i-(k+1)]);
-        }
-        return ans;
+        else v.push_back({'E',tmp[i].second-tmp[i].first});
+     }
+     if(tmp.back().second<eventTime){
+        v.push_back({'G',eventTime-tmp.back().second});
+     }
+    //  for(auto it:v){
+    //     cout<<it.first<<" "<<it.second<<endl;
+    //  }
+
+    int l=0;
+    int m=v.size();
+    int cntevent=0;
+    int gsm=0;
+    int ans=0;
+    for(int i=0;i<m;i++){
+      if(v[i].first=='G') gsm+=v[i].second;
+      else cntevent++;
+      while(cntevent>k){
+        if(v[l].first=='G') gsm-=v[l].second;
+        else cntevent--;
+        l++;
+      }
+      ans=max(ans,gsm);
+    }
+
+     return ans;
     }
 };
