0cdad05bbdd137197b934c88ac9ae5d5f599117f
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-07-10 18:03:18 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (12.22%) - LeetSync

---
+++ 983-validate-stack-sequences/validate-stack-sequences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+         int n=pushed.size(),m=popped.size();
+         if(n!=m) return false;
+         set<int>st;
+         stack<int>v;
+         int pos2=0,pos1=0;
+         while(true){
+            while(pos1<n and st.find(popped[pos2])==st.end()){
+                st.insert(pushed[pos1]);
+                v.push(pushed[pos1]);
+                pos1++;
+
+            }
+            if(st.find(popped[pos2])==st.end()) return false;
+            if(v.top()==popped[pos2]){
+                st.erase(v.top());
+                v.pop();
+                pos2++;
+                if(pos2==n) return true;
+            }
+            else return false;
+         }
+        
+        return false;
+    }
+};
