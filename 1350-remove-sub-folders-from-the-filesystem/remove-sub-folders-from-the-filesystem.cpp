5907b4aaaf8f4981faa12201ab5775307c8c8539
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-07-19 12:40:55 +0530

    Time: 47 ms (84.09%) | Memory: 55.1 MB (53.55%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        // Sort the folders lexicographically so parent folders come before their subfolders
-        sort(folder.begin(), folder.end());
 
-        // Initialize result vector with the first folder
-        vector<string> ans;
-        ans.push_back(folder[0]);
-
-        // Iterate through remaining folders starting from index 1
-        for(int i = 1; i < folder.size(); i++) {
-            // Get the last added folder path and add a trailing slash
-            // This helps in comparing if current folder is a subfolder
-            string lastFolder = ans.back();
-            lastFolder.push_back('/');
-
-            // Compare current folder with last added folder
-            // compare(0, lastFolder.size(), lastFolder) checks if folder[i] starts with lastFolder
-            // If it doesn't start with lastFolder (returns != 0), then it's not a subfolder
-            if(folder[i].compare(0, lastFolder.size(), lastFolder) != 0) {
-                // If not a subfolder, add to result
-                ans.push_back(folder[i]);
+        sort(folder.begin(),folder.end(),[&](string &a, string &b){
+            return a.length()<b.length();
+        });
+        vector<string>ans;
+        unordered_set<string>st;
+        for(auto it:folder){
+            string tmp="";
+            bool f=false;
+            for(auto c:it){
+                if(c=='/'){
+                if(st.find(tmp)!=st.end()){
+                    f=true;
+                    break;
+                }
+                }
+                tmp.push_back(c);
+                
+            }
+            if(!f){
+                ans.push_back(tmp);
+                st.insert(tmp);
             }
         }
-
         return ans;
     }
 };
