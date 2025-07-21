304303a5ac2310a4b6d785e9d4c6e64dc72209a5
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-07-21 11:06:00 +0530

    Time: 42 ms (19.36%) | Memory: 44.6 MB (39.72%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     string makeFancyString(string s) {
+        int n=s.length();
         string ans="";
-        char prev='-';
-        int cnt=1;
-        for(char c: s){
-          if(c==prev){
-
-             if(cnt<2){
-                ans+=c;
-                cnt++;
-             }   
-          }else{
-             ans+=c;
-            prev=c;
-            cnt=1;
-          }
+        for(int i=0;i<n;i++){
+            if(i>=2){
+             if(s[i]==ans.back() and s[i]==ans[ans.size()-2]){
+                ans.pop_back();
+                ans.push_back(s[i]);
+             }
+             else ans.push_back(s[i]);
+            }
+            else ans.push_back(s[i]);
         }
         return ans;
     }
