b09f2f6eb3f75af097da331474dd12abc9c10d0c
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-07-30 11:01:30 +0530

    Time: 3 ms (42.01%) | Memory: 86 MB (65.24%) - LeetSync

---
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxi=*max_element(nums.begin(),nums.end());
+        int ans=1;
+        int cnt=0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==maxi){
+                cnt++;
+            }
+            else{
+                cnt=0;
+            }
+            ans=max(ans,cnt);
+        }
+        return ans;
+    }
+};
