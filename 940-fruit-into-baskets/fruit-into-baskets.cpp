8f6a8e51b6b25fbc5ea44af0cd3ae66fadf43dc9
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-08-04 14:47:24 +0530

    Time: 523 ms (5.25%) | Memory: 135.4 MB (8.05%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -4,15 +4,28 @@ public:
         unordered_map<int,int>mp;
         int n=ft.size();
         int ans=0;
-        int l=0;
-        for(int i=0;i<n;i++){
-            mp[ft[i]]++;
-            while(mp.size()>2){
-                mp[ft[l]]--;
-                if(mp[ft[l]]==0) mp.erase(ft[l]);
-                l++;
+        int lo=1,hi=n;
+        auto f=[&](int m)->bool{
+            unordered_map<int,int>mp;
+            for(int i=0;i<m;i++){
+                mp[ft[i]]++;
             }
-            ans=max(ans,i-l+1);
+            if(mp.size()<=2) return true;
+            for(int i=m;i<n;i++){
+                mp[ft[i-m]]--;
+                if(mp[ft[i-m]]==0) mp.erase(ft[i-m]);
+                mp[ft[i]]++;
+                if(mp.size()<=2) return true;
+            }
+            return false;
+        };
+        while(lo<=hi){
+            int m=(lo+hi)>>1;
+            if(f(m)){
+                ans=m;
+                lo=m+1;
+            }
+            else hi=m-1;
         }
         return ans;
     }
