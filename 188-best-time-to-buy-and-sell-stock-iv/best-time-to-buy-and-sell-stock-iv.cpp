0baffe9d34b15c8887b100ded76c00e1af9e882c
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-08-08 03:01:15 +0530

    Time: 828 ms (5.09%) | Memory: 16.3 MB (67.11%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    int maxProfit(int K, vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>>dp(K+1,vector<int>(n+1,0));
-        vector<int>maxi(n,0);
-        int tmp=0;
+    int maxProfit(int k, vector<int>& pr) {
+        int n=pr.size();
+        vector<vector<int>>dp(n+1,vector<int>(k+1,0));
         for(int i=n-1;i>=0;i--){
-            tmp=max(tmp,prices[i]);
-            maxi[i]=tmp;
-        }
-        
-        for(int k=1;k<=K;k++){
-            int cur_maxi=0;
-            for(int i=n-2;i>=0;i--){
-                dp[k][i]=max({dp[k][i],dp[k][i+1],maxi[i+1]-prices[i]});
+            for(int x=1;x<=k;x++){
+                for(int j=i+1;j<n;j++){
+                    if(pr[j]>pr[i]){
+                        dp[i][x]=max(dp[i][x],(pr[j]-pr[i])+dp[j][x-1]);
+                    }
+                    dp[i][x]=max(dp[i+1][x],dp[i][x]);
+                }
             }
-            for(int i=n-1;i>=0;i--){
-                cur_maxi=max(cur_maxi,dp[k][i]+prices[i]);
-                maxi[i]=cur_maxi;
-            }
-
         }
-        // for(int i=0;i<=K;i++){
-        //     for(int k=0;k<n;k++) cout<<dp[i][k]<<" ";
-        //     cout<<endl;
-        // }
-        return dp[K][0];
+        return dp[0][k];
     }
 };
