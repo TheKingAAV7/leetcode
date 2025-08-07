193ae8ce0027fed2aeeacc2cbe78d4a32bcebbda
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-08-08 02:50:32 +0530

    Time: 11 ms (83.77%) | Memory: 85.7 MB (71.94%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<int>dp(n,0);
-        int maxi=prices.back();
-        for(int i=n-2;i>=0;i--){
-            maxi=max(maxi,prices[i]);
-            dp[i]=max({dp[i+1],maxi-prices[i],dp[i]});
-        }
+    int maxProfit(vector<int>& pr) {
+        int n=pr.size();
+        vector<int>pref(n,0),suff(n,0);
         int mini=INT_MAX;
+        int sm1=0;
+        for(int i=0;i<n;i++){
+            sm1=max(sm1,pr[i]-mini);
+            pref[i]=sm1;
+            mini=min(mini,pr[i]);
+        }
+        sm1=0;
+        int maxi=-1e6;
+        for(int i=n-1;i>=0;i--){
+            sm1=max(sm1,maxi-pr[i]);
+            suff[i]=sm1;
+            maxi=max(maxi,pr[i]);
+        }
+        // for(int i:pref) cout<<i<<" ";
+        // cout<<endl;
+        // for(int i:suff) cout<<i<<" ";
+        // cout<<endl;
         int ans=0;
         for(int i=0;i<n;i++){
-            mini=min(mini,prices[i]);
-            ans=max(ans,(prices[i]-mini)+dp[i]);
+            ans=max(ans,pref[i]+suff[i]);
         }
         return ans;
     }
