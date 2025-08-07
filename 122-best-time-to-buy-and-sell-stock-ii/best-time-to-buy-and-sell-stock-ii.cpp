e43fc61f162e70768fefd83b348deb547279cd0b
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-08-08 02:38:59 +0530

    Time: 3 ms (42.93%) | Memory: 18.4 MB (41.02%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<int>dp(n,0);
+    int maxProfit(vector<int>& pr) {
+        int n=pr.size();
+        vector<int>pos(n,-1);
+        stack<int>st;
         int ans=0;
-        for(int i=n-2;i>=0;i--){
-            dp[i]=max((prices[i+1]-prices[i])+dp[i+1],dp[i]);
-            dp[i]=max(dp[i],dp[i+1]);
+        for(int i=0;i<n;i++){
+            while(!st.empty() and pr[i]>pr[st.top()]){
+                pos[st.top()]=i;
+                st.pop();
+            }
+            st.push(i);
         }
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            if(pos[i]!=-1){
+                dp[i]=max(dp[i],(pr[pos[i]]-pr[i])+dp[pos[i]]);
+            }
+            dp[i]=max(dp[i+1],dp[i]);
+            ans=max(ans,dp[i]);
+        }
+        //for(int i:dp) cout<<i<<" ";
         
-        return dp[0];
+        return ans;
     }
 };
