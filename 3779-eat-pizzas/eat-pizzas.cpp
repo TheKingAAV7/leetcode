8d8fff2e270343239c5b31771c1b99d6c3a35947
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-08-27 17:47:51 +0530

    Time: 65 ms (93.05%) | Memory: 180.1 MB (27.10%) - LeetSync

--- 3779-eat-pizzas/eat-pizzas.cpp
+++ 3779-eat-pizzas/eat-pizzas.cpp
@@ -1,22 +1,38 @@
+#define ll long long
 class Solution {
 public:
-    long long maxWeight(vector<int>& piz) {
-       int n=piz.size(); 
-       sort(piz.begin(),piz.end());
-       int cnt=n/4;
-       long long ans=0;
-       int even=cnt/2;
-       int odd=cnt-even;
-       int pos=n-1;
-       while(odd--){
-        ans+=piz.back();
-        piz.pop_back();
-       }
-       int id=piz.size()-2;
-       while(even--){
-        ans+=piz[id];
-        id-=2;
-       }
-       return ans;
+    long long maxWeight(vector<int>& pz) {
+        int n=pz.size();
+        cout<<n<<endl;
+        sort(pz.begin(),pz.end());
+        // for(int i:pz) cout<<i<<" ";
+        // cout<<endl;
+        ll tmp=n/4;
+        if(tmp==1) return pz.back();
+        
+        int od=ceil(1.0*tmp/2.0);
+        int ev=tmp-od;
+        ll ans=0;
+        while(od--){
+            ans+=pz.back();
+            pz.pop_back();
+        }
+        while(ev--){
+            pz.pop_back();
+            ans+=pz.back();
+            pz.pop_back();
+        }
+      
+       
+        return ans;
+
+
+/*
+Stdout
+12
+9 10 11 12 13 15 15 16 18 20 
+
+
+*/
     }
 };
