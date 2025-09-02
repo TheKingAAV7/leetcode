5ef3e94d93601bc0e16cb0d3ca1bc7d923748402
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-09-02 19:21:46 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (74.81%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -5,33 +5,23 @@ public:
             if(a[0]!=b[0]) return a[0]<b[0];
             return a[1]>=b[1];
         });
-        for(auto it:pts) cout<<it[0]<<" "<<it[1]<<endl;
+        //for(auto it:pts) cout<<it[0]<<" "<<it[1]<<endl;
         int n=pts.size();
         int ans=0;
         for(int i=1;i<n;i++){
-            int x=INT_MIN,y=INT_MIN;
+            int x=INT_MAX,y=INT_MAX;
             for(int j=i-1;j>=0;j--){
                 int x1=pts[j][0];
                 int y1=pts[j][1];
                 int x2=pts[i][0];
                 int y2=pts[i][1];
                 if(x1<=x2 and y1>=y2 ){
-                bool possi=true;
-                for(int k=0;k<i;k++){
-                    if(k!=j){
-                    int x=pts[k][0];
-                    int y=pts[k][1];
-                    if(x>=x1 and x<=x2 and y>=y2 and y<=y1){
-                        possi=false;
-                        break;
-                    }
-                    }
+                if(x1<x and y1<y){
+                    x=x1;
+                    y=y1;
+                    ans++;
                 }
-              //  
-                if(possi){
-                   // cout<<j<<" "<<i<<endl;
-                 ans+=1;
-                }
+                
                 }
             }
         }
