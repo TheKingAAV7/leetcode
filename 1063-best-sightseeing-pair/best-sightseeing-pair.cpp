bd1f097fa5698af7de228f9cc91ee07d490823ea
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-09-03 22:07:18 +0530

    Time: 15 ms (6.42%) | Memory: 53.5 MB (5.04%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int maxScoreSightseeingPair(vector<int>& vals) {
-        priority_queue<int>pq;
-        int n=vals.size();
-        int ans=0;
-        for(int i=n-1;i>=0;i--){
-            if(pq.empty()){
-                pq.push(vals[i]-i);
-                continue;
-            }
-            else{
-            ans=max(ans,i+vals[i]+pq.top());
-            pq.push(vals[i]-i);
-            }
+    int maxScoreSightseeingPair(vector<int>& v) {
+        // 8 1 5 2 6
+        // 0 1 2 3 4
+        priority_queue<array<int,2>>pq;
+        int n=v.size();
+      
+        int maxi=0;
+         pq.push({v[n-1]-n+1,n-1});
+        for(int i=n-2;i>=0;i--){
+            int cur=i+v[i];
+              
+                maxi=max(maxi,cur+pq.top()[0]);
+                 pq.push({v[i]-i,i});
+            
+            
         }
-        return ans;
+        return maxi;
+
     }
 };
