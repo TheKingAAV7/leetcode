106f2289745942d4a0c18836f05403309fe2bfc9
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-09-03 17:23:53 +0530

    Time: 128 ms (64.89%) | Memory: 41.1 MB (67.02%) - LeetSync

--- 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& pts) {
-            sort(pts.begin(),pts.end(),[&](vector<int>&a,vector<int>&b)->bool{
+        sort(pts.begin(),pts.end(),[&](vector<int>&a,vector<int>&b)->bool{
             if(a[0]!=b[0]) return a[0]<b[0];
             return a[1]>=b[1];
         });
         //for(auto it:pts) cout<<it[0]<<" "<<it[1]<<endl;
         int n=pts.size();
-        int ans=0;
+        int cnt=0;
         for(int i=1;i<n;i++){
             int x=INT_MAX,y=INT_MAX;
+            int xi=pts[i][0];
+            int yi=pts[i][1];
             for(int j=i-1;j>=0;j--){
                 int x1=pts[j][0];
                 int y1=pts[j][1];
-                int x2=pts[i][0];
-                int y2=pts[i][1];
-                if(x1<=x2 and y1>=y2 ){
-                if(x1<x and y1<y){
-                    x=x1;
-                    y=y1;
-                    ans++;
-                }
-                
+
+                if(x1<=xi and y1>=yi){
+                    if(x1<x and y1<y){
+                        x=x1;
+                        y=y1;
+                        cnt++;
+                    }
                 }
             }
         }
-        return ans;
+        return cnt;
     }
+
 };
