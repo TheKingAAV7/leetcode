b6ef2a6e499f088b89a8ed151af471b5598430f2
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-09-06 19:10:22 +0530

    Time: 858 ms (5.07%) | Memory: 444.4 MB (5.07%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -1,19 +1,50 @@
-#define ll long long 
+#define ll long long
 class Solution {
 public:
     long long minOperations(vector<vector<int>>& q) {
-        ll ans=0;
-      for(auto it:q){
-        ll lef=it[0],rig=it[1];
-        ll sm=0,p=1;
-        for(ll p4=1;p4<=rig;p4*=4){
-            ll pl=max(lef,p4);
-            ll pr=min(rig,p4*4-1);
-            sm+=max(0LL,p*(pr-pl+1));
-            p++;
+        /*
+
+        dividing by 4 shifts the bits by 2..
+        
+        1-3   1
+        4-15  2
+        16-63 3
+            lef max rig min
+         (2-6)  (1-3)  2 3
+        2 3 4 5 6
+        */
+        ll sm=0;
+        for(auto it:q){
+            ll lef=it[0];
+            ll rig=it[1];
+            ll tmp=0;
+            map<ll,ll>mp;
+            for(ll i=1;i<=15;i++){
+                ll rlef=pow(4,i-1);
+                ll rrig=pow(4,i)-1;
+                ll cntlef=max(rlef,lef);
+                ll cntrig=min(rrig,rig);
+                ll diff=(cntrig-cntlef+1);
+                if(diff>=1){
+                    //cout<<cntlef<<" "<<cntrig<<endl;
+                mp[i]=diff;
+                }
+               // cout<<rlef<<" "<<rrig<<" "<<i<<endl;
+                
+            }
+            // cout<<endl;
+            // for(auto it1:mp) cout<<it1.first<<" "<<it1.second<<endl;
+            
+            long long totalLevels = 0;
+            long long maxLevel = 0;
+            for (auto &p : mp) {
+                totalLevels += p.first * p.second;   
+                maxLevel = max(maxLevel, p.first);
+            }
+            tmp = max(maxLevel, (totalLevels + 1) / 2); 
+            sm+=tmp;
         }
-        ans+=(sm+1)/2;
-      }
-      return ans;
+        return sm;
+
     }
 };
