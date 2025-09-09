13f99d71677d81254689a4ad9709a384b87bf491
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-09-09 18:42:45 +0530

    Time: 14 ms (32.43%) | Memory: 9.3 MB (68.56%) - LeetSync

--- 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
@@ -1,21 +1,41 @@
+#define ll long long
 class Solution {
 public:
+    ll mod=1e9+7;
     int peopleAwareOfSecret(int n, int delay, int forget) {
+        /*
+          1-> (2,3)-> (4,5) (5,6)
+              (3,4)
+
+
+        */
         vector<int>dp(n+1,0);
-        int mod=1e9+7;
-        int share=0;
+        int child=forget-delay;
         dp[1]=1;
         for(int i=2;i<=n;i++){
-            if((i-delay)>=0)
-            share= (share%mod+dp[i-delay]%mod)%mod;
-            if((i-forget)>=0)  share = (share - dp[i - forget] + mod) % mod;
-            dp[i]=share;
-        }
-        int res=0;
-        for(int i=n-forget+1;i<=n;i++){
-            res=(res%mod+dp[i]%mod)%mod;
-        }
+            // this ith state can be reached through i-delay
+            // we will just ignore forget for now..
 
-        return res;
+            for(int j=0;j<child;j++){
+                int prev= i-delay-j;
+                //cout<<i<<" "<<prev<<endl;
+                if(prev>=1){
+                   dp[i]=(dp[i]+dp[prev])%mod;
+                }
+
+            }
+            // if(i-delay>=1){
+            //     dp[i]=(dp[i]+dp[i-delay])%mod;
+
+            // }
+        }
+        // for(int i:dp) cout<<i<<" ";
+        // cout<<endl;
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if(i+forget>n) ans=(ans+dp[i])%mod;
+        }
+        return ans;
+
     }
 };
