d1cce648be696533b76468f5b921aa1fc2bd9315
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-09-11 09:37:04 +0530

    Time: 893 ms (5.22%) | Memory: 22.7 MB (5.44%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        //EeOe
-        vector<char>vow;
-        set<char>vowels={'a','e','i','o','u','A','E','I','O','U'};
-        for(char c:s){
-            if(vowels.find(c)!=vowels.end()){
-                vow.push_back(c);
-            }
+        int n=s.length();
+        vector<int>pos;
+        string cp=s;
+        string str="AEIOUaeiou";
+        for(int i=0;i<n;i++){
+            if(str.find(s[i])!=string::npos) pos.push_back(i);
         }
-        sort(vow.begin(),vow.end());
-        int pos=0;
-        for(char &c:s){
-            if(vowels.find(c)!=vowels.end()){
-                c=vow[pos];
-                pos++;
-            }
+        vector<int>tmp(pos.begin(),pos.end());
+        int cnt=0;
+        sort(pos.begin(),pos.end(),[&](int a, int b)->bool{
+            cnt++;
+            return s[a]<s[b];
+        });
+        for(int i:pos) cout<<i<<" ";
+        cout<<endl;
+        cout<<cnt<<endl;
+        for(int i=0;i<tmp.size();i++){
+            s[tmp[i]]=cp[pos[i]];
         }
         return s;
+
+
+        
+
+
     }
 };
