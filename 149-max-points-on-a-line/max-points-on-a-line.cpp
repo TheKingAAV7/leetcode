a46b41d00e3dbd0e6aec47e0af1ef247695388f7
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-10-21 16:26:11 +0530

    Time: 15 ms (91.62%) | Memory: 17.9 MB (43.59%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -6,7 +6,7 @@ public:
 
         calculate the equation
         
-        (x-x1)/(x2-x1)=(y-y1)/(y2-y1)           confirmed.
+        (x-x1)/(y-y1)=(x2-x1)/(y2-y1)           confirmed.
         we have x1, y1, x2,y2, now just put x3 and y3 in above equation and count
 
         (x-2)=(y-3)/(0)
@@ -20,27 +20,22 @@ public:
             ans=max({ans,row[it[0]],col[it[1]]});
         }
         int n=pts.size();
-        if(n==1) return 1;
+        
         for(int i=0;i<n;i++){
+            unordered_map<double,int>mp;
             for(int j=i+1;j<n;j++){
                 double x1=pts[i][0];
                 double y1=pts[i][1];
                 double x2=pts[j][0];
                 double y2=pts[j][1];
-               
-                int cnt=2;
-                for(int k=j+1;k<n;k++){
-                    double x3=pts[k][0];
-                    double y3=pts[k][1];
-                    if((x2-x1)!=0 and (y2-y1)!=0){
-                    double lef=(x3-x1)/(x2-x1);
-                    double rig=(y3-y1)/(y2-y1);
-                    if(lef==rig) cnt++;
-                    }
+                if(y2!=y1){
+                    double slope=(x2-x1)/(y2-y1);
+                    mp[slope]++;
                 }
-                ans=max(ans,cnt);
-            }
+            } 
+            for(auto it:mp) ans=max(ans,it.second+1);
         }
+        
         return ans;
         
     }
