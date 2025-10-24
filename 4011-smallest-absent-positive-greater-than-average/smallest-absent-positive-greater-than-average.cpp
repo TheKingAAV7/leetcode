855189d270939b96f753724dcb8171ae976d2db6
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-10-24 23:48:21 +0530

    Time: 22 ms (42.40%) | Memory: 92.8 MB (12.13%) - LeetSync

--- 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.cpp
+++ 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int smallestAbsent(vector<int>& nums) {
-        int n=nums.size();
-        double sm=0;
-        for(int i:nums) sm+=(i);
-        
-        int avg=floor(sm/(n*1.0)) + 1;
-        if(avg<=0) avg=1;
-        
-        
-      
-        while(true){
-            bool pres=false;
-            for(int i=0;i<n;i++){
-                if(nums[i]==avg){
-                    pres=true;
-                    break;
-                }
-            }
-            if(!pres)  return avg;
-            avg+=1;
+        int sm=0;
+        for(int i:nums){
+            sm+=(double)(i);
         }
-        return -1;
+        int start=1;
+        
+        int n=nums.size();
+        double cand=1.0*(sm)/n;
+        int tmp= ceil(cand);
+        if(ceil(cand)==floor(cand)) tmp++;
+        start=max(start,tmp);
+        set<int>st(nums.begin(),nums.end());
+        while(st.find(start)!=st.end()){
+            start++;
+        }
+        return start;
+
     }
 };
