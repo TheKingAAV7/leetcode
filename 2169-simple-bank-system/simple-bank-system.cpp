2ab783372f91dd51b706424746af83dee6c04b69
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-10-26 09:26:25 +0530

    Time: 9 ms (72.82%) | Memory: 123.7 MB (60.18%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.cpp
+++ 2169-simple-bank-system/simple-bank-system.cpp
@@ -1,45 +1,41 @@
+#define ll long long
 class Bank {
-private:
-vector<long long>b;
-int n;
 public:
-    Bank(vector<long long>& balance) {
-      n=balance.size();
-      b=balance;  
+    vector<ll>arr;
+    int n;
+    Bank(vector<ll>& balance) {
+        arr=balance;  
+        n=arr.size();      
     }
     
     bool transfer(int a1, int a2, long long money) {
-        a1--;
-        a2--;
-        if(a1>=0 and a1<n and a2>=0 and a2<=n){
-            if(b[a1]>=money){
-                b[a1]-=money;
-                b[a2]+=money;
-                return true;
-            }
-            return false;
+        a1--;a2--;
+        if(a1>=0 and a1<n and a2>=0 and a2<n){
+        if(arr[a1]>=money){
+            arr[a1]-=money;
+            arr[a2]+=money;
+            return true;
+        }
         }
         return false;
     }
     
-    bool deposit(int ac, long long money) {
-        ac--;
-        if(ac>=0 and ac<n){
-        b[ac]+=money;
+    bool deposit(int a, long long money) {
+        if(a>=1 and a<=n){
+        arr[a-1]+=money;
         return true;
         }
         return false;
     }
     
-    bool withdraw(int ac, long long money) {
-        ac--;
-        if(ac>=0 and ac<n){
-        if(b[ac]>=money){
-            b[ac]-=money;
+    bool withdraw(int a, long long money) {
+        a--;
+       if(a>=0 and a<n){
+        if(arr[a]>=money) {
+            arr[a]-=money;
             return true;
         }
-        return false;
-        }
+       }
         return false;
     }
 };
