7a90788094870adcfd690189417303eea1f6a3bf
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-10-30 11:16:48 +0530

    Time: 0 ms (100.00%) | Memory: 77 MB (65.87%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
-    int minNumberOperations(vector<int>& t) {
-        int ans=t[0];
-        for(int i=1;i<t.size();i++){
-            ans+=max(0,t[i]-t[i-1]);
-        }
-        return ans;
+    int minNumberOperations(vector<int>& arr) {
+/*
+    Basically, WE will find ranges for all the elements
+
+    there can be multiple ranges for each element
+     e.g 4-> (0,2) (4,5)
+     map<int,vector<array<int,2>>>mp;
+     now for each element in map sorted, just use sweepline, to determine, what's the 
+     effective value of current element
+
+*/
+    int ans=arr[0];
+    int n=arr.size();
+    for(int i=1;i<n;i++){
+        ans+=max(0,arr[i]-arr[i-1]);
+    }
+    return ans;
     }
 };
