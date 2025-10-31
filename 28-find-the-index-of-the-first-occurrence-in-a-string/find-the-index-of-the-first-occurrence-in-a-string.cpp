9c3e4bb6ce64f5c0b9a87b4732ede3ac22e936d2
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-10-31 19:28:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (12.76%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,32 +1,54 @@
+#define ll long long
 class Solution {
 public:
-    int strStr(string s, string pat) {
-        
-        int m=pat.length();
-        string str= pat + "#" + s;
-        int n=str.length();
-        vector<int>z(n,0);
-        int l=0,r=0;
-        for(int i=1;i<n;i++){
-            if(i<=r){
-                z[i]=min(r-i+1,z[i-l]);
-            }
+        ll mod=1e9+7;
+        ll p=31;
 
-            while(str[z[i]]==str[i+z[i]]) z[i]++;
-                if(z[i]+i-1>r){
-                    r=z[i]+i-1;
-                    l=i;
-                }
+    int strStr(string s, string pat) {
+        ll n=s.length();
+        ll m=pat.length();
+        
+        if(m>n) return -1; 
+        vector<ll>po(n+1,0);
+        po[0]=1;
+        for(int i=1;i<n;i++){
+            po[i]= (po[i-1]*p)%mod;
         }
-        // cout<<str<<endl;
-        // for(int i:z) cout<<i<<" ";
+        ll hshp=0;
+
+        for(int i=0;i<m;i++){
+            ll val = (po[i]*(pat[i]-'a'+1))%mod;
+            hshp= (hshp + val)%mod;
+        }
+        vector<ll>hsh(n,0);
+
+        hsh[0]= (s[0]- 'a' + 1) ;
+        for(int i = 1 ; i < n ; i++){
+            ll val= (po[i]* (s[i]-'a'+1))%mod;
+            hsh[i]= ( hsh[i-1] + val)%mod;
+        }
+        // for(int i:po) cout<<i<<" ";
         // cout<<endl;
-        int pos=0;
-        for(int i=m+1;i<n;i++){
-            if(z[i]==m) return pos;
-            pos++;
+        // for(int i:hsh) cout<<i<<" ";
+        // cout<<endl;
+        // cout<<hshp<<endl;
+
+        for(int i=0;i<n;i++){
+            if(i==0){
+                ll cur= hsh[i+m-1];
+                if(cur==hshp) return 0;
+            }
+            else{
+                if(i+m-1>=n) continue;
+                ll rig= hsh[i+m-1];
+                ll lef= hsh[i-1];
+                ll sub_hash= (rig-lef+mod)%mod;
+                if(sub_hash==((hshp*po[i])%mod)) return i;
+            }
         }
         return -1;
-
+        
+        return -1;
+        
     }
 };
