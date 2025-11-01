d8c3ffa866a2add71b4dd2b18667bffc2e308dd8
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-11-01 14:01:45 +0530

    Time: 2596 ms (5.00%) | Memory: 43.9 MB (67.06%) - LeetSync

---
+++ 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.cpp
@@ -0,0 +1,82 @@
+class Solution {
+public:
+    
+    long long MOD=1e9+7;
+    
+    int zigZagArrays(int n, int l, int r) {
+         int dp[2001][2001][2] ={0};
+         int pref[2001][2] = {0};
+        /*
+
+        Problem:
+        1) Each element belongs to [l,r]
+        2) No two consecutive elements will be same -> they will either be
+        increasing or decreasing.
+        3) No three elements are increasing or decreasing strictly..
+
+        count the no. of ways.. SUMMATION
+        dp[i][l][x] -> i am at index i, the current choosen element is l..
+            x denotes, whether it's increasing or decreasing
+            0-> increasing
+            1-> decreasing
+            transition:
+            dp[i][l][0]= dp[i+1][x][1] ( x belongs to [l+1..r] )
+
+            when i == n-1, dp[i][l..r][0..1]=1;
+        Obs:
+        Solution:
+
+        let's take sum of all  pref[l..r]
+        */
+        // long long dp[n+1][r+1][2];
+        // long long pref[r+1][2];
+    //     vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(r+1,vector<int>(2,0)));
+    //    vector<vector<int>>pref(r+1,vector<int>(2,0));
+    
+        for(int i=l;i<=r;i++){
+            for(int j=0;j<2;j++){
+             pref[i][j]= (pref[i-1][j]+1)%MOD;
+             dp[n-1][i][j]=1;
+             
+            }
+        }
+
+
+        
+        for(int i=n-2;i>=0;i--){
+            for(int x=l;x<=r;x++){
+                        
+                    //  for(int j=l;j<=r;j++){
+                    //     if(j!=x){
+                    //         if(x<j) // incr
+                    //         dp[i][x][0] += dp[i+1][j][1];
+                    //         else dp[i][x][1] += dp[i+1][j][0]; //decr
+                    //     }
+                    //  }
+
+                    int dec_sm = pref[x-1][0];  // up sm 
+                    //down sum of 1
+                    int inc_sm = (pref[r][1]-pref[x][1]+MOD)%MOD;                 
+                     dp[i][x][0] = inc_sm;
+                     dp[i][x][1] = dec_sm;
+            }
+
+            for(int x=l;x<=r;x++){
+            for(int j=0;j<2;j++){
+             pref[x][j]= (pref[x-1][j]%MOD + dp[i][x][j]%MOD)%MOD;
+            }
+         }
+
+        }
+        
+
+    
+        int ans=0;
+        for(int i=l;i<=r;i++){
+            for(int j=0;j<2;j++) ans= (ans%MOD + dp[0][i][j]%MOD )%MOD;
+        }
+        return ans%MOD;
+
+        
+    }
+};
