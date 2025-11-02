68904d6b18c9390208b89a09c8bfc909c2faf471
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-11-02 07:57:11 +0530

    Time: 143 ms (37.62%) | Memory: 195.1 MB (34.40%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -1,100 +1,77 @@
-// union find class with Size & components
-class UnionFind {
-    int root[100001], Size[100001];
-public:
-    int components;
-    //Constructor using initializer list
-    UnionFind(int n): components(n+1){
-        fill(Size, Size+n+1, 1);
-        iota(root, root+n+1, 0);//[0,1,...,n]    
-    }
-    
-    int Find(int x) {//Path Compression O(alpha(n))
-        if (x==root[x]) 
-            return x;
-        return root[x] = Find(root[x]);
-    }
-
-    bool Union(int x, int y) { //Union by Size O(alpha(n))   
-        x=Find(x), y=Find(y);
-        
-        if (x == y) return 0;
-        
-        if (Size[x] > Size[y]) {
-            Size[x] +=Size[y];
-            root[y] = x;
-        } 
-        else {
-            Size[y] += Size[x];
-            root[x] = y;
-        }       
-        components--;
-        return 1;
-    }     
-};
-
 class Solution {
 public:
-    bitset<100000> grid=0;
-    int m, n, N;
-    inline int  idx(int r, int c){
-        return r*n+c;
-    }
-    void cross(int r, int c, UnionFind& G) {
-        // downwards
-        for (int i = r + 1; i < m; i++) {
-            if (grid[idx(i, c)]) break;
-            G.Union(idx(i, c), N); // connect to N
-        }
-        // upwards
-        for (int i = r - 1; i >= 0; i--) {
-            if (grid[idx(i, c)]) break;
-            G.Union(idx(i, c), N); // connect to N
-        }
-        // rightwards
-        for (int j = c + 1; j < n; j++) {
-            if (grid[idx(r, j)]) break;
-            G.Union(idx(r, j), N); // connect to N
-        }
-        // leftwards
-        for (int j = c - 1; j >= 0; j--) {
-            if (grid[idx(r, j)]) break;
-            G.Union(idx(r, j), N); // connect to N
-        }
-    }
+    int countUnguarded(int n, int m, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(auto it:guards) vis[it[0]][it[1]]=1;
+        for(auto it:walls) vis[it[0]][it[1]]=2;
 
-    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        this->m = m, this->n = n;
-        N=n*m;
-        UnionFind G(N);
+        for(int i=0;i<n;i++){
+            bool g=false;
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==1){
+                    g=true;
+                    continue;
+                }
+                if(vis[i][j]==2){
+                    g=false;
+                    continue;
+                }
+                if(g) vis[i][j]=3;
+            }
+        }
+        for(int i=0;i<n;i++){
+            bool g=false;
+            for(int j=m-1;j>=0;j--){
+                if(vis[i][j]==1){
+                    g=true;
+                    continue;
+                }
+                if(vis[i][j]==2){
+                    g=false;
+                    continue;
+                }
+                if(g) vis[i][j]=3;
+            }
+        }
 
-        // Mark walls
-        for (auto& ij : walls){
-            int r = ij[0], c = ij[1];
-            grid[idx(r, c)] = 1;
-            G.components--;
+        for(int j=0;j<m;j++){
+            bool g=false;
+            for(int i=0;i<n;i++){
+                if(vis[i][j]==1){
+                    g=true;
+                    continue;
+                }
+                if(vis[i][j]==2){
+                    g=false;
+                    continue;
+                }
+                if(g) vis[i][j]=3;
+            }
         }
-        // Mark guards 
-        for (auto& ij : guards){
-            int r = ij[0], c = ij[1];
-            grid[idx(r, c)] = 1;
-            G.components--;
+
+        for(int j=0;j<m;j++){
+            bool g=false;
+            for(int i=n-1;i>=0;i--){
+                if(vis[i][j]==1){
+                    g=true;
+                    continue;
+                }
+                if(vis[i][j]==2){
+                    g=false;
+                    continue;
+                }
+                if(g) vis[i][j]=3;
+            }
         }
-        // connect guarded Cells to N
-        for (auto& ij : guards) {
-            int r = ij[0], c = ij[1];
-            cross(r, c, G);
+
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0) ans++;
+            }
         }
-        return G.components-1;
+        return ans;
+
+
     }
 };
-
-
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
-
