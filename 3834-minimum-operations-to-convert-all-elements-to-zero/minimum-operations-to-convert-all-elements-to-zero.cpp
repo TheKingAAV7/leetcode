e9402781df894a9a74475d9c080be79fa3cc2043
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-11-10 15:32:04 +0530

    Time: 767 ms (12.92%) | Memory: 444.1 MB (7.58%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -1,40 +1,69 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        stack<int>st;
-        int pos1=0;
+        /*
+
+        3 1 2 1
+        3 1 2 1 2 2 2 2 1
+        0 1 2 3 4 5 6
+        2 2 3 4 5 4 2 
+        4 5 3 2 1
+        (0-7) 
+
+        WE WILL ALWAYS HAVE TO CHOOSE SUBARRAY SUCH THAT IT CONTAINS ALL THE SMALLEST ELEMENTS
+
+        JUST COUNT HOW MANY DISTINCT RANGES ARE THERE FOR EACH ELEMENT..
+    
+        THE ANSWER WILL BE ATLEAST NO. OF UNIQU ELEMENTS..
+
+        */
+
+        int n = nums.size();
+        vector<array<int,2>>st1,st2;
+        vector<int>pref(n,0),suff(n,0);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int cnt=1;
+            while(!st1.empty() and nums[i]<=st1.back()[0]){
+                cnt+=st1.back()[1];
+                st1.pop_back();
+            }
+            pref[i]=cnt;
+            st1.push_back({nums[i],cnt});  
+        }
+
+        for(int i=n-1;i>=0;i--){
+            int cnt=1;
+            while(!st2.empty() and nums[i]<=st2.back()[0]){
+                cnt+=st2.back()[1];
+                st2.pop_back();
+            }
+            suff[i]=cnt;
+            st2.push_back({nums[i],cnt});  
+        }
+
+        // for(int i:pref) cout<<i<<" ";
+        // cout<<endl;
+        // for(int i:suff) cout<<i<<" ";
+        // cout<<endl;
+
+        unordered_map<int,set<pair<int,int>>>mp;
+
+        for(int i=0;i<n;i++){
+            int lef= i-pref[i] +1;
+            int rig= i+suff[i] -1;
+           // cout<<lef<<" "<<rig<<endl;
+            mp[nums[i]].insert({lef,rig});
+        }
+        for(auto &it:mp){
+            if(it.first==0) continue;
+            ans+=it.second.size();
+        }
+        return ans;
+
         
-        int n=nums.size();
-        while(pos1<n and nums[pos1]==0){
-            pos1++;
-        }
-        if(pos1==n) return 0;
-        int pos2=n-1;
-        while(pos2>=0 and nums[pos2]==0){
-            pos2--;
-        }
-        int cnt=0;
-        for(int i=pos1;i<=pos2;i++){
-           if(st.empty()){
-            st.push(nums[i]);
-            cnt++;
-            continue;
-           }
-           while(!st.empty() and nums[i]<st.top()){
-            st.pop();
-           }
-           if(!st.empty() and st.top()!=nums[i]){
-            st.push(nums[i]);
-            if(nums[i]>0)
-           cnt++;
-           }
-           else if(st.empty()){
-            st.push(nums[i]);
-            if(nums[i]>0)
-            cnt++;
-           }
-            
-        }
-        return cnt;
+
+        
+        
     }
 };
