d0f8380453c3cb8e8ef73ea9470dd615e264875c
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-11-21 21:51:01 +0530

    Time: 673 ms (11.01%) | Memory: 234.5 MB (5.17%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        int n = s.size();
-        vector<int> first(26, -1), last(26, -1);
-
-        // Record first and last occurrences of each character
-        for (int i = 0; i < n; i++) {
-            if (first[s[i] - 'a'] == -1)
-                first[s[i] - 'a'] = i;
-            last[s[i] - 'a'] = i;
+        int n=s.length();
+        //  a s  s a 
+        map<pair<int,int>,int>mp;
+        vector<vector<int>>suff(n,vector<int>(26,0));
+        vector<int>arr(26,0);
+        for(int i=n-1;i>=0;i--){
+            suff[i]=arr;
+            arr[s[i]-'a']++;
+            
         }
 
-        set<string> st;
+        int ans=0;
+        vector<int>pref(26,0);
+        for(int i=0;i<n;i++){
 
-        // Iterate over all characters to count palindromic subsequences
-        for (int i = 0; i < 26; i++) {
-            if (first[i] != -1 && first[i] < last[i]) {
-                // Use a boolean array to track characters between first and last occurrences
-                vector<bool> seen(26, false);
-                for (int j = first[i] + 1; j < last[i]; j++) {
-                    seen[s[j] - 'a'] = true;
-                }
-                // Construct palindromic subsequences
-                for (int j = 0; j < 26; j++) {
-                    if (seen[j]) {
-                        string l = "";
-                        l += (i + 'a');
-                        l += (j + 'a');
-                        l += (i + 'a');
-                        st.insert(l);
+            for(int k=0;k<26;k++){
+                if(pref[k] and suff[i][k]){
+                    if(mp.find({s[i]-'a',k})==mp.end()){
+                        mp[{s[i]-'a',k}]=1;
+                        ans++;
                     }
                 }
             }
+            pref[s[i]-'a']++;
+
         }
 
-        return st.size();
+        return ans;
+
+
     }
 };
