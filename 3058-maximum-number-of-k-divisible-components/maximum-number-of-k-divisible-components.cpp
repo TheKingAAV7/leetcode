9a496f75e1d1e5ee572026b90c790a4d70a5f7f5
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-11-28 18:28:50 +0530

    Time: 307 ms (5.34%) | Memory: 191.1 MB (16.73%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,36 +1,47 @@
 class Solution {
 public:
-    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& vals, int k) {
-        vector<vector<int>> graph(n);
-        vector<int> degree(n);
-        if (n < 2) return 1;
-        for (auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
-            degree[edge[0]]++;
-            degree[edge[1]]++;
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        /*
+
+        1) we can remove an edge only and only if both the components formed 
+        have sm%k==0 
+        2) Let's root the tree at a particular node
+           we have to make sure that whenever an edge is removed, the 
+           condition is satisified..
+
+           we just care if our child edge is satisfying this condition or not..
+           if yes remove it , DON"T CARE
+
+           Requirements: 
+           1) Subtree sum , which we can get in the dfs only right? yes..
+        */
+        vector<vector<int>>adj(n);
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
 
-        vector<long long> nodeVal(vals.begin(), vals.end());
-        queue<int> leafQ;
-        for (int i = 0; i < n; i++) 
-            if (degree[i] == 1) leafQ.push(i);
-
-        int compCnt = 0;
-        while (!leafQ.empty()) {
-            int curr = leafQ.front();
-            leafQ.pop();
-            degree[curr]--;
-            long long carry = 0;
-            if (nodeVal[curr] % k == 0) compCnt++;
-            else carry = nodeVal[curr];
-            for (int nbr : graph[curr]) {
-                if (degree[nbr] == 0) continue;
-                degree[nbr]--;
-                nodeVal[nbr] += carry;
-                if (degree[nbr] == 1) leafQ.push(nbr);
+        function<array<int,2>(int,int)>dfs=[&](int node, int par)->array<int,2>{
+           
+           int cnt=0;
+           int sm=values[node]%k;
+           for(auto it:adj[node]){
+            if(it!=par){
+                array<int,2>tmp=dfs(it,node);
+                int cnt1= tmp[0];
+                int sm1= tmp[1];
+                sm= (sm+sm1)%k;
+                cnt+=cnt1;
+                if(sm1%k==0){
+                   cnt+=1; 
+                }
             }
-        }
-        return compCnt;
+           }
+           return {cnt,sm};
+        };
+
+       auto ans= dfs(0,-1);
+        return ans[0]+1;
+
     }
 };
