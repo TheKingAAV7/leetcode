6b6847ab148988f105ecd35c692d569eaab86171
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-11-30 13:11:28 +0530

    Time: 175 ms (16.08%) | Memory: 90.5 MB (22.25%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,36 +1,26 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-        long totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
+       int n=nums.size();
+       long long sm= accumulate(nums.begin(),nums.end(),0ll);
+
+       long long req= sm%(1ll*p);
+       if(sm%p==0) return 0;
+      // cout<<req<<endl;
+       int ans=INT_MAX;
+       map<long long,int>mp;
+       int curmod=0;
+       mp[curmod]= -1;
+       for(int i=0;i<n;i++){
+
+        curmod=(curmod+nums[i])%p;
+        if(mp.find( (curmod-req+p)%p)!=mp.end()) {
+            int l =mp[ (curmod-req+p)%p];
+            if(i-l!=n)
+            ans=min(ans,i-l);
         }
-
-        // Find the remainder when total sum is divided by p
-        int rem = totalSum % p;
-        if (rem == 0) return 0; // If the remainder is 0, no subarray needs to be removed
-
-        unordered_map<int, int> prefixMod;
-        prefixMod[0] = -1;  // Initialize for handling full prefix
-        long prefixSum = 0;
-        int minLength = nums.size();
-
-        for (int i = 0; i < nums.size(); ++i) {
-            prefixSum += nums[i];
-            int currentMod = prefixSum % p;
-            int targetMod = (currentMod - rem + p) % p;
-
-            if (prefixMod.find(targetMod) != prefixMod.end()) {
-                minLength = min(minLength, i - prefixMod[targetMod]);
-            }
-
-            prefixMod[currentMod] = i;
-        }
-
-        return minLength == nums.size() ? -1 : minLength;
+        mp[curmod]=i;
+       }
+       return ans==INT_MAX?-1:ans;
     }
 };
