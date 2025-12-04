5c1efcb92965570d536fd66c1556cef9078563ec
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-12-04 19:56:18 +0530

    Time: 67 ms (8.12%) | Memory: 24 MB (6.67%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
@@ -1,42 +1,54 @@
 class Solution {
 public:
     int countCollisions(string s) {
-        stack<char> st; // creating a stack
-        int n = s.size();
-        if(n == 1)return 0; // edge case !
-        st.push(s[0]); // Push the very first element in stack 
-        int ans = 0;
-        for(int i=1; i<n; i++)
-        {
-            if(!st.empty() && st.top() == 'R' && s[i] == 'L') // First case 
-            {
-                ans += 2;
-                st.pop();   // remove the last element from the stack because they have collide 
-                st.push('S'); // after collide the car become stationary so, push "S"
+        int n=s.length();
+        stack<char>st;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(st.empty()){
+                st.push(s[i]);
+                continue;
             }
-            else if(!st.empty() && st.top() == 'R' && s[i] == 'S') // second case
-            {
-                ans += 1;
-                st.pop();   // same here 
-                st.push('S');
+            if(s[i]=='L'){
+                // RS
+                if(st.top()=='R'){
+                    ans+=2;
+                    st.pop();
+                    while(!st.empty() and st.top()=='R'){
+                        ans+=1;
+                        st.pop();
+                    }
+                    st.push('S');
+                    st.push('S');
+                }
+                else if(st.top()=='S'){
+                    ans+=1;
+                    st.push('S');
+                }
+                else{
+                    st.push(s[i]);
+                }
             }
-            else if(!st.empty() && st.top() == 'S' && s[i] == 'L') // third case 
-                ans += 1; 
-            else
-                st.push(s[i]); // if no condition is matched push the current element;
-        } 
-        // handling 4th case 
-        while(!st.empty() && st.top() == 'R') // remove the leading R's
-            st.pop();
-        while(!st.empty()) // count the remaing R's
-        {
-            if(st.top() == 'R')ans++;
-            st.pop();
+            else if(s[i]=='S'){
+                if(st.top()=='L'){
+                    st.push(s[i]);
+                }
+                else if(st.top()=='R'){
+                    while(!st.empty() and st.top()=='R'){
+                        ans+=1;
+                        st.pop();
+                    }
+                    st.push(s[i]);
+                }
+                else{
+                    st.push('S');
+                }
+            }
+            else{
+                //LS
+              st.push(s[i]);
+            }
         }
-        // please dry  run this case of your own for better understand
-        // S S R S S R L L R S L L R S R S S R L R R R R L L R R L S S R R
-        
-        
         return ans;
     }
 };
