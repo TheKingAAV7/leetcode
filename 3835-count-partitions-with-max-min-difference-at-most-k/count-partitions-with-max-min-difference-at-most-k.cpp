9acd75f27706837059d15e42298c54c38b0b1adc
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-12-06 14:55:15 +0530

    Time: 264 ms (22.43%) | Memory: 220 MB (32.71%) - LeetSync

--- 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.cpp
+++ 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.cpp
@@ -1,44 +1,66 @@
-
-#define ll long long
 class Solution {
 public:
-    int countPartitions(vector<int>& nums, int k) {
-        int mod=1e9+7;
-        int n=nums.size();
-        vector<ll>pref(n,0);
-        vector<int>dp(n,0);
-        vector<int>idx(n,0);
-        // iota(idx.begin(),idx.end(),0);
+    int countPartitions(vector<int>& arr, int k) {
+        /*
+
+        It's dp
+
+        let dp[i] represents the no. of partitions when start at index i
+
+        each index, have it's corresponding maximum valid index where (maxi-mini)<=k
+
+        how to find this index
+
+        keep a map l and r
+        whenenver the condition is unsatisfied..
+        we gotta assign the previous index to the l
+        l++
+
+        transition
+        dp[i]=dp[i+1]+dp[i+2]..dp[idx]
+
+        use prefix sum to optimize
+        */
+        int n=arr.size();
+        vector<int>pos(n,n-1);
+      //  iota(pos.begin(),pos.end(),0);
         map<int,int>mp;
         int l=0;
         for(int i=0;i<n;i++){
-           mp[nums[i]]++;
-            while(((mp.rbegin()->first)-(mp.begin()->first))>k){
-                mp[nums[l]]--;
-                if(mp[nums[l]]==0) mp.erase(nums[l]);
+            mp[arr[i]]++;
+            while((mp.rbegin()->first-mp.begin()->first)>k){
+                mp[arr[l]]--;
+                if(mp[arr[l]]==0) mp.erase(arr[l]);
+                pos[l]=i-1;
                 l++;
             }
-            idx[i]=l;
         }
-        dp[0]=1;
-        pref[0]=1;
-       
-        for(int i=1;i<n;i++){
-           
-            if(idx[i]==0){
-               dp[i]=(pref[i-1]+1)%mod;
-            }
-            else{
-                if(idx[i]==1){
-                    dp[i]=pref[i-1]%mod;
-                }
-                else
-                dp[i]=(pref[i-1]-pref[idx[i]-2]+mod)%mod;
-            }
-            
-            pref[i]=(pref[i-1]%mod+dp[i]%mod)%mod;
+        // for(int i:pos) cout<<i<<" ";
+        // cout<<endl;
+
+        vector<int>dp(n+1,0);
+        vector<int>pref(n+2,0);
+        int mod=1e9+7;
+        dp[n-1]=1;
+        dp[n]=1;
+        pref[n-1]=2;
+       pref[n]=1;
+        for(int i=n-2;i>=0;i--){
+            int idx= pos[i];
+            int lef=i+1;
+            int rig=idx+1;
+            int tmp = (pref[lef]-pref[rig+1]+mod)%mod;
+           dp[i]= tmp;
+            //cout<<tmp<<endl;
+            pref[i]=(tmp+pref[i+1])%mod;
+            // for(int j=i;j<=idx;j++){
+            //     dp[i]=(dp[i]+dp[j+1])%mod;
+            // }
         }
-     
-        return dp[n-1];
+        // for(int i:dp) cout<<i<< " ";
+        // cout<<endl;
+        // for(int i:pref) cout<<i<<" ";
+        // cout<<endl;
+        return dp[0];
     }
 };
