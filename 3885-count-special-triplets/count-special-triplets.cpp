5bd480281c219675efa12114a94319bf1cee92cd
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-12-09 09:24:51 +0530

    Time: 1515 ms (10.21%) | Memory: 445.9 MB (10.95%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -6,20 +6,20 @@ public:
       int mod = 1e9+7;
       int ans=0;
       vector<int>pref(n,0);
-      map<int,int>mp;
+      map<int,int>mp,mp1;
       for(int i=0;i<n;i++){
         int req= nums[i]*2;
         pref[i]=mp[req];
         mp[nums[i]]++;
       }
-      mp.clear();
+      
       for(int i=n-1;i>=0;i--){
         int req= nums[i]*2;
-        int suff= mp[req];
+        int suff= mp1[req];
         long long x= ((1ll*pref[i])%mod * (1ll*suff)%mod)%mod;
         ans=(ans%mod + x%mod )%mod;
 
-        mp[nums[i]]++;
+        mp1[nums[i]]++;
 
       }
       return ans;
