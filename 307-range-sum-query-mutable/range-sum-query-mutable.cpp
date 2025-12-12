9190b91961bba08ca19f75b462403c7965c368a7
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-12-12 21:48:37 +0530

    Time: 98 ms (19.07%) | Memory: 179.4 MB (96.75%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,65 +1,49 @@
 class NumArray {
-private:
-vector<int>segtree,nums;
-int n;
-
-void up(int i, int lef, int rig, int idx, int val){
-    if(lef==rig){
-        segtree[i]=val;
-        nums[lef]=val;
-        return;
-    }
-    int mid=(lef+rig)/2;
-    if(idx<=mid){
-        up(2*i+1,lef, mid, idx, val);
-    }
-    else up(2*i+2, mid+1, rig, idx, val);
-
-    segtree[i]=segtree[2*i+1]+segtree[2*i+2];
-    return;
-}
-int bt(int idx, int lef, int rig, vector<int>&nums){
-    if(lef==rig){
-        segtree[idx]=nums[lef];
-        return segtree[idx];
-    }
-    int mid=(lef+rig)/2;
-    int left=bt(2*idx+1,lef,mid,nums);
-    int right=bt(2*idx+2,mid+1,rig,nums);
-    segtree[idx]=left+right;
-    return segtree[idx];
-}
-
-int sm(int idx, int lef, int rig,int l, int r){
-    if(lef==l and rig==r){
-        return segtree[idx];
-    }
-    int mid=(lef+rig)/2;
-    if(lef<=l and r<=mid){
-        return sm(2*idx+1,lef,mid,l,r);
-    }
-    if((mid+1)<=l and r<=rig){
-        return sm(2*idx+2,mid+1,rig,l, r);
-    }
-    int left=sm(2*idx+1,lef,mid,l,mid);
-    int right=sm(2*idx+2,mid+1,rig,mid+1,r);
-    return left+right;
-}
 public:
-    NumArray(vector<int>& nums1) {
-        nums=nums1;
+
+    vector<int>arr;
+    vector<int>dp;
+    int B;
+    int n;
+    NumArray(vector<int>& nums) {
+        arr=nums;
         n=nums.size();
-        segtree.resize(4*n,0);
-        bt(0,0,n-1,nums);
+        B=ceil(sqrt(1.0*n));
+        dp.resize(B);
+        for(int i=0;i<n;i++){
+            int block= i/B;
+            dp[block]+= arr[i];
+        }
     }
     
     void update(int index, int val) {
-        up(0,0,n-1,index,val);
+        int block= index/B;
+        dp[block]-= arr[index];
+        arr[index]=val;
+        dp[block]+=arr[index];
         return;
     }
     
     int sumRange(int left, int right) {
-       return  sm(0,0,n-1,left,right);
+        int ans=0;
+        int LBlock= left/B;
+        int RBlock= right/B;
+        if(LBlock==RBlock){
+            for(int i=left;i<=right;i++) ans+=arr[i];
+            return ans;
+        }
+
+        for(int i=LBlock+1;i<RBlock;i++) ans+=dp[i];
+        
+        int LefStart= left;
+        int LefEnd= ((left/B) * B) + (B-1);
+        int RigStart= (right/B) * B;
+        int RigEnd= right;
+        for(int i=LefStart;i<=LefEnd;i++) ans+=arr[i];
+        for(int i=RigStart;i<=RigEnd;i++) ans+=arr[i];
+
+        return ans;
+
     }
 };
 
@@ -69,12 +53,3 @@ public:
  * obj->update(index,val);
  * int param_2 = obj->sumRange(left,right);
  */
-
-    #define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() {
-  struct ___ { static void _() { std::ofstream("display_runtime.txt") << 0  << '\n'; } };
-  std::atexit(&___::_);
-  return 0;
-}();
-#endif
