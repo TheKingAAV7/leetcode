c12ba4f1370fd19089515d53fa1e6a09d0c1f79a
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-12-12 21:58:03 +0530

    Time: 80 ms (25.41%) | Memory: 183.2 MB (18.39%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,49 +1,57 @@
 class NumArray {
 public:
-
-    vector<int>arr;
-    vector<int>dp;
-    int B;
+    vector<int>st,arr;
     int n;
-    NumArray(vector<int>& nums) {
-        arr=nums;
-        n=nums.size();
-        B=ceil(sqrt(1.0*n));
-        dp.resize(B);
-        for(int i=0;i<n;i++){
-            int block= i/B;
-            dp[block]+= arr[i];
+
+    int query(int idx, int lef, int rig, int l, int r){
+        if(lef==l and rig==r) return st[idx];
+
+        int mid= (lef+rig)>>1;
+        if(r<=mid) return query(2*idx+1,lef,mid,l,r);
+        if((mid+1)<=l) return query(2*idx+2,mid+1,rig,l,r);
+        int left= query(2*idx+1,lef,mid,l,mid);
+        int right= query(2*idx+2,mid+1,rig,mid+1,r);
+        return left+right;
+    }
+
+    void up(int idx, int lef, int rig, int i, int val){
+        if(lef==rig){
+            arr[lef]=val;
+            st[idx]=val;
+            return;
         }
+        int mid=(lef+rig)>>1;
+        if(i<=mid) up(2*idx+1,lef,mid,i,val);
+        else up(2*idx+2,mid+1,rig,i,val);
+        st[idx]= st[2*idx+1] + st[2*idx+2];
+        return;
+    }
+
+    void build(int idx, int lef, int rig){
+        if(lef==rig){
+            st[idx]=arr[lef];
+            return;
+        }
+        int mid=(lef+rig)>>1;
+        build(2*idx+1,lef,mid);
+        build(2*idx+2,mid+1,rig);
+        st[idx]=st[2*idx+1] + st[2*idx+2];
+        return;
+    }
+    NumArray(vector<int>& nums) {
+        n=nums.size();
+        arr=nums;
+        st.resize(4*n);
+        build(0,0,n-1);
     }
     
     void update(int index, int val) {
-        int block= index/B;
-        dp[block]-= arr[index];
-        arr[index]=val;
-        dp[block]+=arr[index];
+        up(0,0,n-1,index,val);
         return;
     }
     
     int sumRange(int left, int right) {
-        int ans=0;
-        int LBlock= left/B;
-        int RBlock= right/B;
-        if(LBlock==RBlock){
-            for(int i=left;i<=right;i++) ans+=arr[i];
-            return ans;
-        }
-
-        for(int i=LBlock+1;i<RBlock;i++) ans+=dp[i];
-        
-        int LefStart= left;
-        int LefEnd= ((left/B) * B) + (B-1);
-        int RigStart= (right/B) * B;
-        int RigEnd= right;
-        for(int i=LefStart;i<=LefEnd;i++) ans+=arr[i];
-        for(int i=RigStart;i<=RigEnd;i++) ans+=arr[i];
-
-        return ans;
-
+        return query(0,0,n-1,left,right);
     }
 };
 
