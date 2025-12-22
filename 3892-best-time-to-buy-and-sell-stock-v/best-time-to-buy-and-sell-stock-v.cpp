6bdb5d246e7888d1f4ac74d9580a22fb1db2e922
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2025-12-22 23:20:19 +0530

    Time: 935 ms (18.97%) | Memory: 345.7 MB (6.42%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
@@ -1,64 +1,45 @@
-#define ll long long
-/*
-WE can optimize this by maintaining maximum and minimum values..
-
-
-*/
+typedef long long ll;
+// T.C and S.C are O(N∗K)
 class Solution {
 public:
-    long long maximumProfit(vector<int>& prices, int K) {
-        #define LC_HACK
-        #ifdef LC_HACK
-        const auto __=[](){
-            struct ___{
-                static void _(){
-                    ofstream("display_runtime.txt") << 0 << "\n";
-                }
-            };
-            std::atexit(&___::_);
-            return 0;
-        }();
-        #endif
+ll mini = -1e14;
+ll calcul( vector<int>& prices, vector<vector<vector<ll>>>&dp, int i, int k, int type ){
+if( i == prices.size() ){
+if( type == 0 ) return 0;
+else return mini;
+}
 
-        #if defined(_GNUC_)
-        #include <bits/allocator.h>
-        #pragma GCC optimize("Ofast,unroll-loops")
-        #pragma GCC target("avx2,popcnt")
-        #endif
-        
-        int n=prices.size();
-        vector<vector<ll>>dp(n+1,vector<ll>(K+2,0));
-        vector<vector<ll>>mx1(n+1,vector<ll>(K+2,-1e12));
-        vector<vector<ll>>mx2(n+1,vector<ll>(K+2,-1e12));
-        
-        // mn mx
-        for(int k=1;k<=K;k++){ 
-        mx1[n-1][k]=prices.back();
-        mx2[n-1][k]= -prices.back();
-        }
-        for(int i=n-2;i>=0;i--){
-            for(int k=1;k<=K;k++){
-                dp[i][k]=max(dp[i][k],dp[i+1][k]);
-                ll mxi= mx1[i+1][k];
-                ll mxj= mx2[i+1][k];
-                dp[i][k]=max(dp[i][k],mxi-prices[i]);
-                dp[i][k]=max(dp[i][k],mxj+prices[i]);
-                mx1[i][k]= max({mx1[i][k],prices[i]+dp[i+1][k-1],mx1[i+1][k]});
-                mx2[i][k]=max({mx2[i][k],dp[i+1][k-1]-prices[i],mx2[i+1][k]});
-                // for(int j=i+1;j<n;j++){  // prices[i]+dp[j+1][k-1]-prices[j]   - prices[j]+dp[j+1][k-1] - prices[i]
-                //     dp[i][k]= max(dp[i][k],abs(prices[j]-prices[i])+dp[j+1][k-1]);
-                
-                    
-                // }
-            }
-        }
-        ll ans=0;
-        for(int i=0;i<n;i++) {
-            // for(int j=1;j<=K;j++) cout<<dp[i][j]<<" ";
-            // cout<<endl;
-            ans=max(ans,dp[i][K]);
-        }
-        // cout<<endl;
-        return ans;
+    if( dp[i][k][type] != mini ) return dp[i][k][type];
+    if( k == 0 && type == 0 ) return 0;
+
+    ll profit = mini;
+    // do nothing
+    profit = max( profit , calcul(prices,dp,i+1,k,type) );
+    // operation_type - 1 
+    if( type == 0 ){
+        // buy stock here
+        profit = max( profit , calcul(prices,dp,i+1,k,1) - 1LL*prices[i] );
+        // sell stock first ( transaction-2 )
+        profit = max( profit , calcul(prices,dp,i+1,k,2) + 1LL*prices[i] );
+    }else if( type == 1 && k > 0 ){
+        // sell it
+        profit = max( profit , calcul(prices,dp,i+1,k-1,0) + 1LL*prices[i]);
+    }else if( type == 2 && k > 0 ){
+        // buy it
+        profit = max( profit , calcul(prices,dp,i+1,k-1,0) - 1LL*prices[i]);
     }
+
+    return dp[i][k][type] = profit;
+
+
+}
+
+long long maximumProfit(vector<int>& prices, int k) {
+    int n = prices.size();
+    vector<vector<vector<ll>>>dp(n+1,vector<vector<ll>>(k+1,vector<ll>(3,mini)));
+    // 0 -> free, 1 -> holding ( transaction-1 ), 2-> sold( transaction-2 )
+
+    ll ans = calcul(prices,dp,0,k,0);
+    return ans;
+}
 };
