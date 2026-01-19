4487a1f7b1d064106cef79d6f56189cfb6797c64
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-01-19 13:57:07 +0530

    Time: 16 ms (45.45%) | Memory: 33 MB (8.18%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -16,7 +16,14 @@ public:
                 pref[i][j]= cur + up + left - common;
             }
         }
-        for(int len=min(n,m);len>0;len--){
+        int lo= 1;
+        int hi= min(n,m);
+
+        
+        while(lo<=hi){
+            int len= (lo+hi)>>1;
+            int mid=len;
+            bool flag=false;
             for(int i=0;i+len-1<n;i++){
                 for(int j=0;j+len-1<m;j++){
                     int dx= i+len-1;
@@ -27,10 +34,19 @@ public:
                     int left= (j-1>=0)? pref[dx][j-1] : 0;
                     int common= (i-1>=0 and j-1>=0)? pref[i-1][j-1]:0;
                     int sm= total-up-left+common;
-                    if(sm<=threshold) return len;
+                    if(sm<=threshold) {
+                        flag= true;
+                        break;
+                    }
                 }
+                if(flag) break;
             }
+            if(flag){
+                ans= len;
+                lo= mid+1;
+            }
+            else hi= mid-1;
         }
-        return 0;
+        return ans;
     }
 };
