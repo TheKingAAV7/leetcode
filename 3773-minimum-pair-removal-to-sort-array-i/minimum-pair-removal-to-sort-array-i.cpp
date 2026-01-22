4a6b9c0e6969c9ab4ac6a796cdfb8a8f03161256
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-01-22 21:41:53 +0530

    Time: 3 ms (40.33%) | Memory: 26.7 MB (41.46%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
@@ -4,25 +4,33 @@ public:
         int n=nums.size();
         int ans=0;
          while(true){
+            // checking if the arary is the non-decreasing, if yes then stop the process or return ans other wise
+            // perform the operation one at a time.
              bool f=false;
              for(int i=1;i<nums.size();i++){
-                 if(nums[i-1]>nums[i]){
+                 if(nums[i-1]>nums[i]){ // this violation of the rule to make the array non-decreasing
                      f=true;
                      break;
                  }
              }
-             if(!f) return ans;
+             if(!f){
+              return ans;
+              } // the array is already sorted in non-decreasing way.
+
+              // the array is unsatisfactory => perform the operation
             int pos=-1;
-            int maxi=INT_MAX;
+            int mini=INT_MAX;
             for(int j=1;j<nums.size();j++){
-                if(nums[j]+nums[j-1]<maxi){
+                if(nums[j]+nums[j-1] < mini){
                     pos=j-1;
-                    maxi=nums[j]+nums[j-1];
+                    mini=nums[j]+nums[j-1];
                 }
             }
+            // we want to erase elements at position j-1 and j
+            // after that we want to insert their sm at j-1 th position which is stored in the pos
              nums.erase(nums.begin()+pos+1);
-             nums.erase(nums.begin()+pos);
-             nums.insert(nums.begin()+pos,(maxi));
+             nums.erase(nums.begin()+pos);         //    1 5 3
+             nums.insert(nums.begin()+pos,(mini));
              ans++;
              
         }
