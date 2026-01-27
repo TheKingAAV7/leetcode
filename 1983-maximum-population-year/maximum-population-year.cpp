584447c52ed1562ba5ab93593c4ac26c50cf68f1
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-01-27 18:56:17 +0530

    Time: 9 ms (4.01%) | Memory: 11.8 MB (6.93%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -2,20 +2,32 @@ class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& events) {
         int n=events.size();
-        map<int,int>mp;
-        for(auto it:events){
-            mp[it[0]]++;
-            mp[it[1]]--;
-        }
-        int maxi=INT_MIN;
+        vector<vector<int>>events1;
+        for(auto it:events) events1.push_back({it[1],it[0]});
+        sort(events.begin(),events.end());
+        sort(events1.begin(),events1.end());
+        int mini=events[0][0];
+        int maxi=events.back()[1];
+        int mx= INT_MIN;
         int ans=-1;
-        int sm=0;
-        for(auto it:mp){
-            sm+=it.second;
-            if(sm>maxi){
-                ans=it.first;
-                maxi=sm;
+        for(int i=mini;i<=maxi;i++){
+            // all the elements whose end is < i // i-1 is allowed
+            // and the elements whose start >=i  // >=i is not allowed
+            vector<int>x1={i,INT_MAX}; // lower bound chagne to INT_MAX if doesn't work
+            vector<int>x2={i,INT_MAX}; // lower bound =/=
+            auto it1= lower_bound(events1.begin(),events1.end(),x1);
+            it1--;
+            int idx1= it1-events1.begin();
+            auto it2= upper_bound(events.begin(),events.end(),x2);
+            int idx2= it2-events.begin();
+            int c1= idx1+1;
+            int c2= n-(idx2);
+            int cnt= n-(c1+c2);
+            if(cnt>mx){
+                mx=cnt;
+                ans= i;
             }
+            //cout<<i<<" "<<cnt<<endl;
         }
         return ans;
     }
