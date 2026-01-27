490b0ce759df892147f7429090d243571e6549c3
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-01-27 21:00:58 +0530

    Time: 243 ms (70.91%) | Memory: 302.2 MB (55.29%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
@@ -2,44 +2,54 @@ class Solution {
 public:
     int minCost(int n, vector<vector<int>>& edges) {
         vector<vector<array<int,2>>>adj(n),revadj(n);
-        for(auto it:edges){
-            adj[it[0]].push_back({it[1],it[2]});
-            revadj[it[1]].push_back({it[0],2*it[2]});
+        /*
+        0-> {{1,2},{2,1},{3,5}}
+
+        */
+        for(auto &it:edges){
+            int u= it[0];
+            int v= it[1];
+            int w= it[2];
+            adj[u].push_back({v,w});
+            revadj[v].push_back({u,2*w});
         }
-        // dist,node,flag
-        priority_queue<array<int,3>,vector<array<int,3>>,greater<array<int,3>>>pq;
-        vector<vector<int>>vis(n,vector<int>(2,INT_MAX));
-        pq.push({0,0,0});
-        pq.push({0,0,1});
-        vis[0][0]=0;
-        vis[0][1]=0;
+        // cost,node 
+        vector<int>dist(n,INT_MAX);
+        priority_queue<array<int,2>,vector<array<int,2>>,greater<array<int,2>>>pq; 
+
+        pq.push({0,0});
+        dist[0]=0;
         while(!pq.empty()){
             auto cur= pq.top();
             pq.pop();
-            int dist= cur[0];
+            int cost= cur[0];
             int node= cur[1];
-            int flag= cur[2];
-           // cout<<dist<<" "<<node<<" "<<flag<<endl;
-            if(node==n-1) return dist;
 
+            if(node==n-1) return cost;
+            // either reverse one of edges and traverse it , or traverse the other          nodes normally
+
+            // traverse normal edges first
             for(auto it:adj[node]){
                 int nei= it[0];
                 int wt= it[1];
-                if(dist+wt<vis[nei][flag]){
-                    vis[nei][flag]=dist+wt;
-                    pq.push({dist+wt,nei,flag});
+                int newcost= cost+wt;
+                if(newcost<dist[nei]){
+                    dist[nei]=newcost;
+                    pq.push({newcost,nei});
                 }
             }
-          //  if(flag) continue;
+
+            // traverse reversed edges
             for(auto it:revadj[node]){
                 int nei=it[0];
                 int wt= it[1];
-               // cout<<node<<" "<<nei<<" "<<wt<<" "<<dist<<endl;
-                if(dist+wt<vis[nei][1]){
-                    vis[nei][1]=dist+wt;
-                    pq.push({dist+wt,nei,1});
+                int newcost= cost+wt;
+                if(newcost<dist[nei]){
+                    dist[nei]= newcost;
+                    pq.push({newcost,nei});
                 }
             }
+
         }
         return -1;
     }
