af161d63ef23b53ce023db24b16671831e2cd8d2
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-03 11:10:26 +0530

    Time: 5 ms (4.92%) | Memory: 27.6 MB (96.31%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.cpp
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     bool isTrionic(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=1;i<n;i++){
-            for(int j=i+1;j<n-1;j++){
-                
-                bool one,two,three;
-                one=two=three=true;
-                for(int k=1;k<=i;k++){
-                    if(nums[k]<=nums[k-1]) {
-                        one=false;
+        int  n=nums.size();
+
+        for(int p=1;p<n-1;p++){
+            for(int q=p+1;q<n-1;q++){
+                int inc1=1;
+                int prv=-1;
+                for(int i=1;i<=p;i++){
+                    if(nums[i-1]>=nums[i]){
+                        inc1=false;
                         break;
                     }
                 }
-                for(int k=i+1;k<=j;k++){
-                    if(nums[k]>=nums[k-1]){
-                        two=false;
+                int dec=1;
+                for(int i=p;i<=q;i++){
+                    if(i+1<=q){
+                    if(nums[i]<=nums[i+1]){
+                        dec=0;
                         break;
                     }
-                }
-                for(int k=j+1;k<n;k++){
-                    if(nums[k]<=nums[k-1]) {
-                        three=false;
-                        break;
                     }
                 }
-                //if(i==2 and j==4) cout<<one<<two<<three<<endl; 
-                if(one and two and three) return true;
+                int inc2=1;
+                for(int i=q;i<n-1;i++){
+                 if(nums[i]>=nums[i+1]){
+                        inc2=0;
+                        break;
+                 }
+                }
+                if(inc1 and inc2 and dec) return true;
             }
         }
         return false;
