c792a392e67496d6310097763b32b3a33fd48ca1
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-07 22:00:05 +0530

    Time: 219 ms (30.60%) | Memory: 184.3 MB (21.85%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -1,46 +1,53 @@
 class Solution {
-private:
-void dfs(int node,vector<vector<int>>&adj,vector<int>&disc,vector<int>&low,int &cnt,vector<int>&vis,int parent,vector<vector<int>>&ans){
-    disc[node]=cnt;
-    low[node]=cnt;
-    vis[node]=1;
-    for(auto it:adj[node]){
-        if(vis[it]==0){
-            ++cnt;
-         //   cout<<it<<endl;
-            dfs(it,adj,disc,low,cnt,vis,node,ans);
-        }
-    }
-    for(auto it:adj[node]){
-        if(it!=parent){
-            low[node]=min(low[it],low[node]);
-        }
-    }
-    if(parent!=-1){
-        if(disc[parent]<low[node]){
-            ans.push_back({parent,node});
-        }
-    }
-    return;
-}
 public:
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& con) {
-        vector<vector<int>>ans;
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& edges) {
         vector<vector<int>>adj(n);
-        
-        for(auto it: con){
+        for(auto it:edges){
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-        vector<int>disc(n,-1),low(n,INT_MAX);
-        int cnt=1;
-        vector<int>vis(n,0);
-        int parent=-1;
+
+        vector<int>lvl(n,0);
+        vector<int>dp(n,0);
+
+        vector<vector<int>>ans;
+        vector<int>isbridge(n,0);
+
+        function<void(int,int,int)>dfs=[&](int node,int level,int par)->void{
+            lvl[node]=level;
+            
+            for(auto nei:adj[node]){
+                if(lvl[nei]==0){
+                    dfs(nei,level+1,node);
+                    dp[node]+=dp[nei];
+                }
+                else if(lvl[nei]>lvl[node]){
+                    // going down => subtrack
+                    dp[node]--;
+                }
+                else{
+                    // going up => add
+                    dp[node]++;
+                }
+            }
+            dp[node]--;
+            if(lvl[node]>1 and dp[node]==0){
+                ans.push_back({par,node});
+            }
+            return;
+          
+        };
         for(int i=0;i<n;i++){
-            if(vis[i]==0){
-        dfs(i,adj,disc,low,cnt,vis,parent,ans);
+            if(!lvl[i]){
+                dfs(i,1,-1);
             }
         }
+       
         return ans;
+
+        
+
+
+
     }
 };
