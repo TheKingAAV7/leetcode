732deec4e24c609aad1dd46ba835a68717a27b19
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-21 12:42:19 +0530

    Time: 15 ms (31.08%) | Memory: 16.8 MB (5.09%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
+    vector<vector<vector<vector<int>>>>dp;
     unordered_set<int>st={2,3,5,7,11,13,17,19,23,29,31};
     int f(int i, int leading_zero,int tight,int cnt,int &len,string &s){
         if(i>=len){
             if(st.find(cnt)!=st.end()) return 1;
             return 0;
         }
+        if(dp[i][leading_zero][tight][cnt]!=-1) return dp[i][leading_zero][tight][cnt];
         int mx= tight?(s[i]-'0') : 1;
         int ans=0;
         for(int cur=0;cur<=mx;cur++){
@@ -13,12 +15,13 @@ public:
                 ans+=f(i+1,leading_zero && (cur==0), tight && (cur==mx),cnt+cur,len,s);
             
         }
-        return ans;
+        return dp[i][leading_zero][tight][cnt]=ans;
 
     }
 
     int caller(int num){
         if(num==-1) return 0;
+
 
         string s;
         for(int i=0;i<31;i++){
@@ -27,6 +30,7 @@ public:
         }
         reverse(s.begin(),s.end());
         int len=31;
+          dp.assign(len, vector<vector<vector<int>>>(2, vector<vector<int>>(2, vector<int>(32, -1))));
         
         return f(0,1,1,0,len,s);
         
