dd0cf7bb8ac60910025109b2ca17a5e18d4c301e
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-21 12:35:20 +0530

    Time: 1301 ms (5.00%) | Memory: 8.5 MB (7.11%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,13 +1,44 @@
 class Solution {
 public:
-    int countPrimeSetBits(int left, int right) {
-        unordered_set<int>st={2,3,5,7,11,13,17,19,23,29,31};
-        int ans=0;
-        for(int i=left;i<=right;i++){
-            int tmp=i;
-            int cnt=__builtin_popcount(tmp);
-            if(st.find(cnt)!=st.end()) ans++;
+    unordered_set<int>st={2,3,5,7,11,13,17,19,23,29,31};
+    int f(int i, int leading_zero,int tight,int cnt,int &len,string &s){
+        if(i>=len){
+            if(st.find(cnt)!=st.end()) return 1;
+            return 0;
         }
+        int mx= tight?(s[i]-'0') : 1;
+        int ans=0;
+        for(int cur=0;cur<=mx;cur++){
+            
+                ans+=f(i+1,leading_zero && (cur==0), tight && (cur==mx),cnt+cur,len,s);
+            
+        }
+        return ans;
+
+    }
+
+    int caller(int num){
+        if(num==-1) return 0;
+
+        string s;
+        for(int i=0;i<31;i++){
+            if((num>>i)&1) s.push_back('1');
+            else s.push_back('0');
+        }
+        reverse(s.begin(),s.end());
+        int len=31;
+        
+        return f(0,1,1,0,len,s);
+        
+    }
+    int countPrimeSetBits(int left, int right) {
+
+        // state-> idx,leading_zero,tight,cnt,  
+
+
+
+        int ans= caller(right) - caller(left-1);
+       
         return ans;
     }
 };
