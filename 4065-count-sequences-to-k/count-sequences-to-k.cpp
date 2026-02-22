559d33035b0df12fee8076836f272061c405eca7
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-22 20:11:58 +0530

    Time: 186 ms (91.04%) | Memory: 42.9 MB (91.55%) - LeetSync

--- 4065-count-sequences-to-k/count-sequences-to-k.cpp
+++ 4065-count-sequences-to-k/count-sequences-to-k.cpp
@@ -1,31 +1,64 @@
-#define ll long long
+
+#define ll __int128
 class Solution {
 public:
-    int countSequences(vector<int>& nums, long long k) {
-        int n=nums.size();
-        long double K= k;
-        const long double EPS = 1e-12L;
-        map<pair<int,long double>,long long>dp;
-        vector<int>vreq= {5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5};
-        long long valk=30517578125;
-        if(k==valk and vreq==nums) return 6954;
 
-        auto f=[&](auto&& self,int i,int op,long double val)->long long{
-            if(i>=n){
-                return fabsl(val - K) <= EPS;   // changed
+    static __int128 abs128(__int128 x){ return x < 0 ? -x : x; }
+    static __int128 gcd128(__int128 a, __int128 b){
+    a = abs128(a); b = abs128(b);
+    while (b != 0){
+        __int128 t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+}
+
+    int countSequences(vector<int>& nums, long long k) {
+        int n= nums.size();
+        int lef= n/2;
+        map<pair<ll,ll >,int>mp;
+        
+        int ans=0;
+
+        auto f= [&](auto &&self, int i, int sz, int toggle, ll n1, ll d1 ){
+            if(i>=sz){
+                if(toggle==0){
+               
+                ll gc= gcd128(n1,d1);
+                n1/=gc;
+                d1/=gc;
+                mp[{n1,d1}]++;
+                return;
+                }
+                else{
+                    
+                   ll reqnum= d1*k;
+                   ll reqden= n1;
+                   ll gc= gcd128(reqnum,reqden);
+                   reqnum/=gc;
+                   reqden/=gc;
+                   ans+=mp[{reqnum,reqden}];
+                   
+                }
+                return; 
             }
-            if(dp.find({i,val})!=dp.end()) return dp[{i,val}];
-            long long ans=0;
-            long double x= nums[i];
-            ans+=self(self,i+1,op,val*x);
-            if(fabsl(x) > EPS)                 // guard division by zero
-                ans+=self(self,i+1,op,val/x);
-            ans+=self(self,i+1,op,val);
-            return dp[{i,val}]=ans;
+            self(self,i+1,sz,toggle,n1*nums[i],d1);
+            self(self,i+1,sz,toggle,n1,d1*nums[i]);
+            self(self,i+1,sz,toggle,n1,d1);
+            return ;
         };
 
-        long double val=1.0;
 
-        return f(f,0,0,val);
+
+        ll n1,d1;
+        n1=d1=1;
+        f(f,0,lef,0,n1,d1);
+        // cout<<endl;
+        //val=1;
+        f(f,lef,n,1,n1,d1);
+        
+        return ans;
+        
     }
 };
