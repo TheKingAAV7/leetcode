72f358ced88d05bfb12ba2c13d60ea846647d3a3
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-22 15:42:54 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (44.66%) - LeetSync

--- 899-binary-gap/binary-gap.cpp
+++ 899-binary-gap/binary-gap.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int binaryGap(int n) {
-        int curpos=0;
         int prev=-1;
         int ans=0;
-        while(n>0){
-            if(n&1){
-                if(prev!=-1)
-                ans=max(ans,curpos-prev);
-                prev=curpos;
+        for(int i=0;i<30;i++){
+            // check if the ith bit set or not
+            if(((n>>i)&1)==1){
+                
+                // this is a set bit
+                // calculate the distance
+                int distance;
+                if(prev!=-1){
+                distance= i-prev;
+               // cout<<distance<<endl;
+                ans=max(ans,distance);
+                }
+                prev=i;
             }
-            curpos++;
-            n>>=1;
         }
         return ans;
     }
