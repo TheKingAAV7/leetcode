e9902d412688745ceb1f562a1bb65dc4c486ad8b
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-25 12:50:56 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (10.53%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
+    static bool comp(int &a, int &b ){
+        // index of a > index of b
+        int cnt1= __builtin_popcount(a);
+        int cnt2= __builtin_popcount(b);
+        if(cnt1<cnt2) return true;
+        if(cnt1==cnt2) return a<b;
+        return false;
+    }
     vector<int> sortByBits(vector<int>& arr) {
         /*
 
         Bubble Sort
+        Custom sorting
         */
 
         int n=arr.size();
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int num1= arr[i];
-                int num2= arr[j];
-                int cnt1= __builtin_popcount(arr[i]);
-                int cnt2= __builtin_popcount(arr[j]);
-                if(cnt1>cnt2){
-                    swap(arr[i],arr[j]);
-                }
-                else if(cnt1==cnt2){
-                    if(arr[i]>arr[j]){
-                        swap(arr[i],arr[j]);
-                    }
-                }
-            }
-        }
+        sort(arr.begin(),arr.end(),comp);
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         int num1= arr[i];
+        //         int num2= arr[j];
+        //         int cnt1= __builtin_popcount(arr[i]);
+        //         int cnt2= __builtin_popcount(arr[j]);
+        //         if(cnt1>cnt2){
+        //             swap(arr[i],arr[j]);
+        //         }
+        //         else if(cnt1==cnt2){
+        //             if(arr[i]>arr[j]){
+        //                 swap(arr[i],arr[j]);
+        //             }
+        //         }
+        //     }
+        // }
+
         return arr;
     }
 
