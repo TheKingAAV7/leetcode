c34e5d09e354625a6082b53862d2c5711588791f
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-26 13:25:46 +0530

    Time: 205 ms (32.94%) | Memory: 132.8 MB (8.74%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
+    int MAXN;
+    vector<int>v;
+    vector<int>pref;
+    Solution(){
+        
+    }
     int countPrimes(int n) {
-        vector<int>p(n,1);
-        if(n<2) return 0;
-        p[0]=0;
-        p[1]=0;
-        for(long long i=2;i<n;i++){
-            if(p[i]){
-                for(long long j=i*i;j<n;j+=i){
-                    p[j]=0;
-                }
+        if(n==0) return 0;
+        MAXN=n;
+
+        v.assign(MAXN,1);
+        if(n<=2) return 0;
+        v[1]=0;
+        v[0]=0;
+        int cnt=0;
+        for(int i=2;i<MAXN;i++){
+            if(v[i]){
+                for(int j=2*i;j<MAXN;j+=i) v[j]=0;
             }
+            if(v[i]) 
+            cnt++;
+          
         }
-        int ans=0;
-        for(int i=2;i<n;i++){
-            if(p[i]) ans++;
-        }
-        return ans;
+
+        return cnt;
     }
 };
