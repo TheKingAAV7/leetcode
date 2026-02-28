e41ba6787e49128fd7a8f5e27f726045cc8e2300
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-02-28 23:39:30 +0530

    Time: 1 ms (74.36%) | Memory: 30.8 MB (72.68%) - LeetSync

--- 4231-smallest-pair-with-different-frequencies/smallest-pair-with-different-frequencies.cpp
+++ 4231-smallest-pair-with-different-frequencies/smallest-pair-with-different-frequencies.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> minDistinctFreqPair(vector<int>& nums) {
         int n=nums.size();
         map<int,int>mp;
-        for(int i:nums) mp[i]++;
-        for(auto it:mp){
-            for(auto it1:mp){
+        for(int &i:nums) mp[i]++;
+        for(auto &it:mp){
+            for(auto &it1:mp){
                 if(it.first!=it1.first){
                     if(it1.second!=it.second) return {it.first,it1.first};
                 }
