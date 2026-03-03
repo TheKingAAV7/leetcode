cf09d94c514bc064f3cc07264502fb516e638d9e
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-03-03 12:40:16 +0530

    Time: 3 ms (44.29%) | Memory: 8 MB (46.53%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
+
+    
     char findKthBit(int n, int k) {
-
-
-        char ans;
-        function<void(string&,int,int&)>f=[&](string &s, int n,int& k ){
-            if(n==0){
-                ans=s[k-1];
-                return;
-            }
-            
-            string tmp= s;
-            reverse(tmp.begin(),tmp.end());
-            for(char &c: tmp){
-                if(c=='0') c='1';
-                else c='0';
-            }
-            string nstr = s + "1" + tmp;
-            f(nstr,n-1,k); 
-            return;
+        k--;
+        function<char(int,int)>f=[&](int n, int k)->char{
+            cout<<n<<" "<<k<<endl;
+            if(n==1) return '0';
+            int len= (1<<n)-1;
+            if(k<len/2) return f(n-1,k);
+            if(k==len/2) return '1';
+            char cur= f(n-1,len-k-1);
+            return cur=='0'?'1':'0';
         };
 
-        string cur="0";
-        if(n==1) return cur[0];
-        f(cur,n,k);
-        return ans;
+        return f(n,k);
+
     }
 };
