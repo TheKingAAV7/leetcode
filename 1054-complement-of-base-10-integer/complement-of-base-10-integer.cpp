34f4859cd9637f0cc627d7bbfc01e1c79ba226c5
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-03-11 10:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (26.22%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int bitwiseComplement(int n) {
         if(n==0) return 1;
-        int sz= floor(log2(double(n)))+1;
+        int sz= log2(n)+1;
       for(int i=sz-1;i>=0;i--){
         n^=(1<<i);
       }
