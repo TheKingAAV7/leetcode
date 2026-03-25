0bb80aab681d6a5299f3b15afdc79831695932eb
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-03-25 16:03:24 +0530

    Time: 214 ms (5.02%) | Memory: 101.7 MB (5.02%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.cpp
+++ 310-minimum-height-trees/minimum-height-trees.cpp
@@ -1,46 +1,75 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        if (n == 1) return {0};
-    
-        std::vector<std::list<int>> adjacency_list(n);
-        std::vector<int> degree(n, 0);
-        for (auto& edge : edges) {
-            int u = edge[0], v = edge[1];
-            adjacency_list[u].push_back(v);
-            adjacency_list[v].push_back(u);
-            degree[u]++;
-            degree[v]++;
+         vector<vector<int>>adj(n);
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
         
-        std::queue<int> leaves;
-        for (int i = 0; i < n; ++i) {
-            if (degree[i] == 1) leaves.push(i);
-        }
+        vector<int>v(n,0);
+        vector<int>height(n,0);
+        function<int(int,int)>f=[&](int node, int par)->int{
+          
+          int mx=0;
+          for(auto it:adj[node]){
+              if(it!=par){
+                  int curht= f(it,node);
+                  mx=max(mx,curht);
+              }
+          }
+          v[node]=max(v[node],1+mx);
+          return height[node]=1+mx;
+        };
         
-        int remainingNodes = n;
-        while (remainingNodes > 2) {
-            int leavesCount = leaves.size();
-            remainingNodes -= leavesCount;
-            for (int i = 0; i < leavesCount; ++i) {
-                int leaf = leaves.front();
-                leaves.pop();
-                for (int neighbor : adjacency_list[leaf]) {
-                    if (--degree[neighbor] == 1) {
-                        leaves.push(neighbor);
-                    }
-                }
-            }
-        }
+        function<void(int,int,int)>f1=[&](int node, int par,int upmx)->void{
+         /*
+         store {ht,it} in set
+         find answer for child node..
+         
+         v[child]= {1+upmx,max(other children's height)+1}
+         return max(upmx,max(other children's height))
+         */
+         
+         v[node]=max(v[node],upmx);
+         upmx++;
+         set<array<int,2>>st;
+         map<int,int>mp;
+         for(auto it:adj[node]){
+             if(it!=par){
+                 st.insert({height[it],it});
+                 mp[it]=height[it];
+             }
+         }
+         for(auto it:adj[node]){
+             if(it!=par){
+                 array<int,2>todel={mp[it],it};
+               
+                 st.erase(todel);
+                 
+                 int lst=0;
+                 if(st.size()>0)
+                 lst=(*prev(st.end()))[0];
+                 f1(it,node,max(upmx,lst+1));
+                 st.insert(todel);
+             }
+         }
+         return;
+        };
         
-        std::vector<int> result;
-        while (!leaves.empty()) {
-            result.push_back(leaves.front());
-            leaves.pop();
+        f(0,-1);
+        for(int &i:v) i--;
+        // for(int i:v) cout<<i<<" ";
+        // cout<<endl;
+        f1(0,-1,0);   
+        // for(int i:v) cout<<i<<" ";
+        // cout<<endl;
+        int mn=*min_element(v.begin(),v.end());
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(v[i]==mn) ans.push_back(i);
         }
+        return ans;
         
-        return result;
     }
 };
