9edc305dec2e97f9cc816483f93f40b4d562148a
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-03-30 08:47:59 +0530

    Time: 90 ms (5.17%) | Memory: 20.2 MB (49.43%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     bool checkStrings(string s1, string s2) {
         int n=s1.length();
-        vector<int> a1(26,0),a2(26,0);
-        vector<int>b1(26,0),b2(26,0);
+        array<int,26> a1={0};
+        array<int,26>a2={0};
+        array<int,26>b1={0};
+        array<int,26>b2={0};
+        // vector<int>b1(26,0),b2(26,0);
         for(int i=0;i<n;i++){
             if(i%2==0){
              a1[s1[i]-'a']++;
@@ -14,7 +17,11 @@ public:
              b2[s2[i]-'a']++;
             }
         }
-       
+        
+        for(int i:b1) cout<<i<<" ";
+        cout<<endl;
+        for(int i:b2) cout<<i<<" ";
+        cout<<endl;
         return a1==a2 and b1==b2;
     }
 };
