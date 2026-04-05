43314a85ca78aaa2a36e722923581c318f186546
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-04-05 07:26:11 +0530

    Time: 0 ms (100.00%) | Memory: 25.6 MB (90.26%) - LeetSync

--- 4255-minimum-absolute-difference-between-two-values/minimum-absolute-difference-between-two-values.cpp
+++ 4255-minimum-absolute-difference-between-two-values/minimum-absolute-difference-between-two-values.cpp
@@ -3,9 +3,17 @@ public:
     int minAbsoluteDifference(vector<int>& nums) {
         int ans=INT_MAX;
         int n=nums.size();
+        int last1,last2;
+        last1=last2=-1;
+
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(nums[i]==1 and nums[j]==2) ans= min(ans,abs(i-j));
+            if(nums[i]==2){
+                if(last1!=-1) ans= min(ans,(i-last1));
+                last2=i;
+            }
+            else if(nums[i]==1){
+                if(last2!=-1) ans= min(ans,(i-last2));
+                last1=i;
             }
         }
         return (ans==INT_MAX)?-1:ans;
