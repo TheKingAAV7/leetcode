6127834e7068d67039fd9e76303da6c815d2b425
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-04-08 14:43:40 +0530

    Time: 116 ms (14.83%) | Memory: 169.2 MB (18.18%) - LeetSync

---
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=nums.size();
+        int MOD=1e9+7;
+        for(auto it:queries){
+            int l=it[0];
+            int r= it[1];
+            int k= it[2];
+            int v= it[3];
+            int idx=l;
+            while(idx<=r){
+                nums[idx]=(1ll*nums[idx]*v)%MOD;
+                idx+=k;
+            }
+        }
+        int ans=0;
+        for(int i:nums) ans^=i;
+        return ans;
+    }
+};
