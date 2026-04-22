d2637b2032e83b44ce8f9dd238ca52bfdbde2858
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-04-22 08:04:58 +0530

    Time: 15 ms (39.04%) | Memory: 19 MB (30.48%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dict) {
+        int n= queries.size();
+        int m= dict.size();
+        vector<string>ans;
+        set<string>st;
+        for(auto s:queries){
+            bool reached= false;
+            for(auto t:dict){
+                if(s.length()!=t.length()) continue;
+                int cnt=0;
+                for(int i=0;i<s.length();i++) cnt+=(s[i]!=t[i]);
+                if(cnt<=2) {
+                 reached= true;
+                 break;
+                }
+            }
+            if(reached){
+                ans.push_back(s);
+            }
+        }
+        return ans;
+    }
+};
