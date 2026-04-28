313a77d10679e4d00484ccd03227669a1c8ce7bb
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-04-28 08:48:44 +0530

    Time: 41 ms (51.85%) | Memory: 87.2 MB (71.19%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        int n=grid.size(),m=grid[0].size();
+        int n= grid.size();
+        int m= grid[0].size();
+        int prev=-1;
         vector<int>v;
-        for(auto it:grid){
-            for(auto i:it) v.push_back(i);
-        }
-        int ans=0;
-        sort(v.begin(),v.end());
-        int tar=v[v.size()/2];
-    
-        for(auto it:grid){
-            for(auto i:it){
-                
-                if((i-tar)%x!=0) {
-                    cout<<i<<endl;
-                return -1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int cur= grid[i][j]%x;
+                v.push_back(grid[i][j]);
+                if(prev==-1){
+                    prev= cur;
                 }
-              // cout<<abs((i-tar)/x)<<endl;
-                ans+=(abs((i-tar)/x));
+                else if(prev!=cur) return -1;
             }
         }
-        
-
+        sort(v.begin(),v.end());
+        n= v.size();
+        int mid= v[n/2];
+        // cout<<mid<<endl;
+        int ans=0;
+        for(int i:v){
+         int diff= abs(i-mid);
+         ans+= diff/x;
+        }
         return ans;
     }
 };
