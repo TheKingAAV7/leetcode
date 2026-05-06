c4621b231cf753ed6f264f302653f0105968ab4f
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-05-06 06:47:59 +0530

    Time: 15 ms (41.52%) | Memory: 59.5 MB (11.30%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-       int m=box[0].size(),n=box.size();
-       for(int i=0;i<n;i++){
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& mat) {
+        int n= mat.size();
+        int m= mat[0].size();
+        for(int i=0;i<n;i++){
+            int lastempty=-1;
+            for(int j=m-1;j>=0;j--){
+                if(mat[i][j]=='*'){
+                    lastempty= j-1;
+                }
+                else if(mat[i][j]=='#'){
+                    if(lastempty!=-1){
+                        swap(mat[i][j],mat[i][lastempty]);
+                        lastempty--;
+                    }
+                    else{
+                        lastempty= j-1;
+                    }
+                }
+                else{
+                    lastempty=max(lastempty,j);
+                }
+            }
+        }
+        vector<vector<char>>ans(m);
+        for(int j=0;j<m;j++){
+            for(int i=n-1;i>=0;i--){
+                ans[j].push_back(mat[i][j]);
+            }
+        }
         
-        for(int j=m-1;j>=0;j--){
-          int b=j+1;
-          int idx=-1;
-          if(box[i][j]=='#'){
-            while(b<m and box[i][b]=='.'){
-                idx=b;
-                b++;
-            }
-            if(idx!=-1){
-                cout<<idx<<endl;
-                box[i][idx]='#';
-                box[i][j]='.';
-            }
-          }  
-        }
-       }
-    int rows = box.size();
-    int cols = box[0].size();
-
-    vector<vector<char>> rotated(cols, vector<char>(rows));
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            rotated[j][rows - 1 - i] = box[i][j];
-        }
-    }
-
-    return rotated;
-      
+        return ans;
     }
 };
