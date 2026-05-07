c30833ca0bad8bc5005c4c46864b45d418d27d7d
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-05-07 18:52:32 +0530

    Time: 15 ms (47.34%) | Memory: 225.8 MB (34.04%) - LeetSync

--- 3981-jump-game-ix/jump-game-ix.cpp
+++ 3981-jump-game-ix/jump-game-ix.cpp
@@ -1,66 +1,51 @@
 class Solution {
 public:
     vector<int> maxValue(vector<int>& nums) {
-        int n=nums.size();
-        vector<array<int,2>>v;
-        int x;
-        map<int,int>mp;
-        vector<int>suff(n,-1);
-        mp[nums.back()]=n-1;
-        suff[n-1]=n-1;
-        for(int i=n-2;i>=0;i--){
-            x=nums[i];
-            auto it=mp.lower_bound(x);
-            if(it==mp.begin()){
-                mp[x]=max(mp[x],i);
-                suff[i]=i;
-            }
-            else{
-                it--;
-                suff[i]=it->second;
-                mp[x]=max({mp[x],it->second,i});
+        int n = nums.size();
+
+        vector<int> prefixMax(n), suffixMin(n);
+
+        prefixMax[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixMax[i] = max(prefixMax[i - 1], nums[i]);
+        }
+
+        suffixMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = min(suffixMin[i + 1], nums[i]);
+        }
+
+        vector<int> ans(n);
+
+        int start = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (prefixMax[i] <= suffixMin[i + 1]) {
+
+                int mx = nums[start];
+
+                for (int j = start; j <= i; j++) {
+                    mx = max(mx, nums[j]);
+                }
+
+                for (int j = start; j <= i; j++) {
+                    ans[j] = mx;
+                }
+
+                start = i + 1;
             }
         }
 
-        for(int i=0;i<n;i++){
-            v.push_back({i,suff[i]});
+        int mx = nums[start];
+
+        for (int j = start; j < n; j++) {
+            mx = max(mx, nums[j]);
         }
-        sort(v.begin(),v.end());
-        vector<array<int,2>>nv;
-        int mini=v[0][0];
-        int maxi=v[0][1];
-        for(int i=1;i<n;i++){
-            int l=v[i][0];
-            int r=v[i][1];
-            if(l>maxi){
-                nv.push_back({mini,maxi});
-                mini=l;
-                maxi=r;
-            }
-            else{
-                maxi=max(maxi,r);
-            }
+
+        for (int j = start; j < n; j++) {
+            ans[j] = mx;
         }
-        nv.push_back({mini,maxi});
-        vector<int>ans;
-        for(auto it:nv){
-            int l=it[0];
-            int r=it[1];
-            int maxi=-1;
-            for(int i=l;i<=r;i++){
-                maxi=max(maxi,nums[i]);
-            }
-            ans.push_back(maxi);
-        }
-        vector<int>res;
-        for(int i=0;i<nv.size();i++){
-             int l=nv[i][0];
-            int r=nv[i][1];
-            int cnt=r-l+1;
-            for(int j=0;j<cnt;j++){
-                res.push_back(ans[i]);
-            }
-        }
-        return res;
+
+        return ans;
     }
 };
