271833f8e4db031b9bd6a977be7888495acf94e0
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-05-14 15:54:15 +0530

    Time: 68 ms (23.81%) | Memory: 31 MB (13.49%) - LeetSync

--- 3384-minimum-number-of-operations-to-make-word-k-periodic/minimum-number-of-operations-to-make-word-k-periodic.cpp
+++ 3384-minimum-number-of-operations-to-make-word-k-periodic/minimum-number-of-operations-to-make-word-k-periodic.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minimumOperationsToMakeKPeriodic(string word, int k) {
         int n= word.size();
-        map<string,int>mp;
+        unordered_map<string,int>mp;
         int ans=0;
         for(int i=0;i<n;i+=k){
             string s;
