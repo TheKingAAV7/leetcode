237ae32326e45b0f7427b74c92c28fa5866c8c63
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-05-17 06:13:45 +0530

    Time: 0 ms (100.00%) | Memory: 36.7 MB (57.86%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
         int n= arr.size();
-        vector<vector<int>>adj(n);
-        for(int i=0;i<n;i++){
-            int lef= i-arr[i];
-            int rig= i+arr[i];
-            if(lef>=0) adj[i].push_back(lef);
-            if(rig<n) adj[i].push_back(rig);
-        }
+        // vector<vector<int>>adj(n);
+       
         queue<int>q;
         vector<int>vis(n,0);
         q.push(start);
         vis[start]=1;
         while(!q.empty()){
-            auto cur= q.front();q.pop();
-            if(arr[cur]==0) return true;
-            for(auto it:adj[cur]){
-                if(!vis[it]){
-                    q.push(it);
-                    vis[it]=1;
-                }
+            auto i= q.front();q.pop();
+            if(arr[i]==0) return true;
+            int lef= i-arr[i];
+            int rig= i+arr[i];
+            if(lef>=0 and !vis[lef]){
+                q.push(lef);
+                vis[lef]=1;
+            } 
+            if(rig<n){
+             q.push(rig);
+             vis[rig]=1;
             }
         }
         return false;
