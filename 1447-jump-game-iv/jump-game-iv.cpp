7b6d32b12757c020de69d222ee38c7591f71b6a6
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-05-18 14:54:55 +0530

    Time: 163 ms (19.39%) | Memory: 83.3 MB (40.54%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.cpp
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -2,49 +2,40 @@ class Solution {
 public:
     int minJumps(vector<int>& arr) {
         int n= arr.size();
-        vector<int>vis(n,INT_MAX);
         map<int,vector<int>>mp;
         for(int i=0;i<n;i++){
             mp[arr[i]].push_back(i);
         }
-
-        /*
-
-        th1= > bring done section to up
-        
-
-
-        */
-        set<int>done;
         queue<int>q;
+        vector<int>vis(n,INT_MAX);
+        set<int>done;
         q.push(0);
         vis[0]=0;
         while(!q.empty()){
-            auto cur= q.front(); q.pop();
+            auto cur= q.front();q.pop();
+            // cout<<cur<<" "<<arr[cur]<<endl;
             if(cur==n-1) return vis[cur];
             int lef= cur-1;
-            int rig= cur+1;
+            int rig=cur+1;
             if(lef>=0 and vis[lef]==INT_MAX){
-                vis[lef]= vis[cur]+1;
+                vis[lef]=1+vis[cur];
                 q.push(lef);
             }
             if(rig<n and vis[rig]==INT_MAX){
-                vis[rig]=vis[cur]+1;
+                vis[rig]=1+vis[cur];
                 q.push(rig);
             }
-            if(done.count(arr[cur])) continue;
-            for(int idx:mp[arr[cur]]){
-                if(idx!=cur){
-                    if(vis[idx]>1+vis[cur]){
-                    vis[idx]= 1+vis[cur];
-                    q.push(idx);
+            int x= arr[cur];
+            if(done.find(x)==done.end()){
+                for(int i:mp[x]){
+                    if(vis[i]==INT_MAX){
+                    vis[i]=1+vis[cur];
+                    q.push(i);
                     }
                 }
+                done.insert(x);
             }
-            done.insert(arr[cur]);
         }
         return -1;
-
-        
     }
 };
