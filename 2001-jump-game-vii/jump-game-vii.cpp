54b98bb141c004e1673f5962f23bb43c5ee059c7
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-05-25 09:51:44 +0530

    Time: 122 ms (5.85%) | Memory: 57.9 MB (5.42%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.cpp
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -1,34 +1,51 @@
+
 class Solution {
 public:
-    bool canReach(string s, int mini, int maxi) {
-      set<pair<int,int>>st;
-      int n=s.length();
-      if(s.back()!='0') return false;
-     vector<bool>pref(n,false);
-     pref[0]=true;
-      for(int i=0;i<n;i++){
-        if(s[i]=='0'){
-            auto it=st.upper_bound({i,INT_MAX});
-            bool f=false;
-            if(it != st.end() && it->first == i) f = true; 
-            if(it != st.begin() && (i >= prev(it)->first && i <= std::prev(it)->second)) f = true;
-
-
-           if(f || pref[i]){
-           // cout<<i<<endl;
-            pref[i]=true;
-           if(i+mini<=n-1 and i+maxi>=n-1) {
-            
-           return true;
-           }
-           st.insert({i+mini,i+maxi});
-           }
+    vector<int>st;
+    void update(int idx, int lef, int rig, int i, int val){
+        if(lef==rig){
+            st[idx]=val;
+            return;
         }
-      }
-      for(auto i:pref) cout<<i<<" ";
-      cout<<endl;
-      
-      return pref[n-1];
+        int mid= (lef+rig)>>1;
+        if(i<=mid) update(2*idx+1,lef,mid,i,val);
+        else update(2*idx+2,mid+1,rig,i,val);
+        st[idx]= st[2*idx+1] | st[2*idx+2]; 
+        return;
+    }
+    int query(int idx, int lef, int rig, int l, int r){
+        if(lef>rig) return 0;
+        if(lef==l and rig==r) return st[idx];
+        int mid= (lef+rig)>>1;
+        if(r<=mid) return query(2*idx+1,lef,mid,l,r);
+        else if((mid+1)<=l) return query(2*idx+2,mid+1,rig,l,r);
+        int left= query(2*idx+1,lef,mid,l,mid);
+        int right= query(2*idx+2,mid+1,rig,mid+1,r);
+        return left|right;
+    }
+    bool canReach(string s, int minJ, int maxJ) {
+        int n=s.length();
+        vector<int>dp(n,0);
+        if(s.back()=='1') return false;
+        st.resize(4*n,0);
+        dp[n-1]=1;
+        update(0,0,n-1,n-1,1);
+        for(int i=n-2;i>=0;i--){
+            if(s[i]=='1') continue;
+            int l= i+minJ;
+            int r= min(n-1,i+maxJ);
+            
+            if(l>r) continue;
+            int cur= query(0,0,n-1,i+minJ,min(n-1,i+maxJ));
+            update(0,0,n-1,i,cur);
+            dp[i]=cur;
+            
 
+            // for(int j=i+minJ;j<=min(n-1,i+maxJ);j++){
+            //     if(dp[j]) dp[i]=1;
+            // }
+        }
+        
+        return dp[0];
     }
 };
