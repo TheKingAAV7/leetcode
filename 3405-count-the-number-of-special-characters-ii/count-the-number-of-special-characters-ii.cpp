1647005a0f3aeadef29395c0b207f039e6b28c33
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-05-26 19:29:51 +0530

    Time: 53 ms (97.51%) | Memory: 24.5 MB (40.88%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -4,7 +4,6 @@ public:
         int n= word.size();
         vector<int>big(26,INT_MAX);
         vector<int>small(26,-1);
-        set<char>st(word.begin(),word.end());
         for(int i=n-1;i>=0;i--){
             if(word[i]>='a' and word[i]<='z'){
                 if(small[word[i]-'a']==-1)
