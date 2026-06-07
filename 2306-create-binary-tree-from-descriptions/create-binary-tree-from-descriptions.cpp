42fb7a0ae950092af55838416426d3fc39d8cb3d
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-06-07 07:18:10 +0530

    Time: 508 ms (5.26%) | Memory: 311.2 MB (8.12%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+
+
+
+    4 3 2 1
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& edges) {
+        map<int,TreeNode*>mp;
+        for(auto it:edges){
+            int par= it[0];
+            int child= it[1];
+            int islef= it[2];
+            TreeNode* p= NULL;
+            TreeNode* c= NULL;
+            if(mp.find(par)!=mp.end()){
+                p=mp[par];
+            }
+            else p= new TreeNode(par);
+            if(mp.find(child)!=mp.end()){
+                c=mp[child];
+            }
+            else c= new TreeNode(child);
+            mp[par]=p;
+            mp[child]=c;
+
+            if(islef) p->left= c;
+            else p->right= c;
+        }
+        vector<TreeNode*>st;
+        set<TreeNode*>vis;
+        auto f=[&](auto &&self,TreeNode* node)->void{
+            if(vis.count(node)) return;
+            vis.insert(node);
+            if(node->left) self(self,node->left);
+            if(node->right) self(self,node->right);
+            st.push_back(node);
+            return;
+        };
+        
+        for(auto it:mp){
+            f(f,it.second);
+        }
+        return st.back();
+    }
+};
