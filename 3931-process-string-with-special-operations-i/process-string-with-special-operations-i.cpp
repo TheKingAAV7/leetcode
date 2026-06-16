6ae4f04b307fe4b9a9938fa3c8a3bd937981930d
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-06-16 07:45:08 +0530

    Time: 3 ms (76.89%) | Memory: 39.7 MB (69.70%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     string processStr(string s) {
-        string ans="";
-        for(char c:s){
-            if(c>='a' and c<='z'){
-                ans.push_back(c);
-            }
-            else if(c=='*'){
-                if(ans.size()>0) ans.pop_back();
+        int n=s.length();
+        string ans;
+        for(char &c:s){
+            if(c=='*'){
+                if(ans.length()) ans.pop_back();
             }
             else if(c=='#'){
                 ans+=ans;
             }
-            else reverse(ans.begin(),ans.end());
+            else if(c=='%') reverse(ans.begin(),ans.end());
+            else ans.push_back(c);
         }
         return ans;
     }
