3767dc1a1da34d4c2c0faa023e76c20d6a623712
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-06-28 10:45:39 +0530

    Time: 6 ms (78.25%) | Memory: 55.4 MB (8.47%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -4,16 +4,19 @@ public:
         sort(arr.begin(),arr.end());
         int n= arr.size();
         arr[0]=1;
+        int ans=1;
         for(int i=1;i<n;i++){
             if(arr[i]>arr[i-1]){
                 arr[i]=arr[i-1]+1;
+                ans=max(ans,arr[i]);
             }
             else{
                 if(arr[i]>arr[i-1])
                 arr[i-1]=arr[i]+1;
                 else arr[i-1]=arr[i];
+                ans=max(ans,arr[i-1]);
             }
         }
-        return *max_element(arr.begin(),arr.end());
+        return ans;
     }
 };
