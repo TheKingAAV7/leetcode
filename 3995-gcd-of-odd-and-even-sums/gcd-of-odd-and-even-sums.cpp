7e64ecded890154234cd226315da70023fe3bace
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-07-15 09:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,9 +1,8 @@
-#define ll long long
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        ll od=n*n;
-        ll ev=n*(n+1);
-        return gcd(od,ev);
+        int sm1= n*n;
+        int sm2= n*(n+1);
+        return gcd(sm1,sm2);
     }
 };
