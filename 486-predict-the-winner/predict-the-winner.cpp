4492c7988c190871cb92f113beeffa4f7b1550c8
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-08-01 10:50:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (85.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n= nums.size();
-
+        int dp[20][20][2];
+        fill(&dp[0][0][0], &dp[0][0][0] + 20*20*2, -1);
         auto f=[&](auto &&self,int lf, int rg, int turn)->int{
             if(lf>rg) return 0;
+            if(dp[lf][rg][turn]!=-1) return dp[lf][rg][turn];
             int diff=0;
             int ans1,ans2;
             if(!turn) {
                 ans1=nums[lf]+self(self,lf+1,rg,!turn);
                 ans2=nums[rg]+self(self,lf,rg-1,!turn);
-                return max(ans1,ans2);
+                return dp[lf][rg][turn]= max(ans1,ans2);
             }
             else{
                 ans1= -nums[lf] + self(self,lf+1,rg,!turn);
                 ans2= -nums[rg] + self(self,lf,rg-1,!turn);
-                return min(ans1,ans2);
+                return dp[lf][rg][turn]=min(ans1,ans2);
             }
             return 0;
 
         };
         int ans= f(f,0,n-1,0);
-        cout<<ans<<endl;
+       
         return ans>=0;
     }
 };
