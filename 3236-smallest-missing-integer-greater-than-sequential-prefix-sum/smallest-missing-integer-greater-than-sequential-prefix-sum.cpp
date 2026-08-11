548599fafdb82d6d6a039aeb2e9603bb8a8faa8f
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-08-11 11:23:25 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (64.41%) - LeetSync

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -6,12 +6,12 @@ public:
         int sm=0;
         int cur= nums[0];
         sm=cur;
-        set<int>st(nums.begin(),nums.end());
+        unordered_set<int>st(nums.begin(),nums.end());
         for(int i=1;i<n;i++){
             if(nums[i]==nums[i-1]+1) sm+=nums[i];
             else break;
         }
-        while(st.count(sm)) sm++;
+        while(st.find(sm)!=st.end()) sm++;
         return sm;
     }
 };
