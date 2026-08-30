3422a6e7513aa5628b4d7d2e6c6b699d12974d46
Sushant Bhoite <130385110+TheKingAAV7@users.noreply.github.com>
2026-08-30 12:15:26 +0530

    Time: 0 ms (100.00%) | Memory: 92.1 MB (22.14%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return 1;
+        int n= nums.size();
         int mini= *min_element(nums.begin(),nums.end());
         int maxi= *max_element(nums.begin(),nums.end());
-        
-        int mn,mx;
-        
+        int p1,p2,p3,p4;
+        p1=p3=INT_MIN;
+        p2=p4=INT_MAX;
         for(int i=0;i<n;i++){
             if(nums[i]==mini){
-                mn=i;
+                p1=max(p1,i);
+                p2=min(p2,i);
             }
-            else if(nums[i]==maxi){
-                mx=i;
+            if(nums[i]==maxi){
+                p3=max(p3,i);
+                p4=min(p4,i);
             }
         }
-        int lef= min(mx,mn);
-        int rig=max(mx,mn);
-        // lef, rig,   only lef, only rig
-        int ans1= (lef+1) + (n-rig);
-        int ans2= rig+1;
-        int ans3= (n-lef);
-        return min({ans1,ans2,ans3});
 
+        // cout<<p1<<" "<<p2<<" "<<p3<<" "<<p4<<endl;
+
+        return min({n - max(abs(p1-p4)-1,abs(p3-p2)-1),max(p1,p3)+1,n-min(p2,p4)});
     }
 };
